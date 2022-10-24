5707549322c88bc1bdee28ae9622a3fcb2333901
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-10-24 00:02:12 -0700

    .

--- send-port.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-import sys
-
-b = input("to where?: ")
-subprocess.call('scp -r  ~/bin/port/* jiuy97@cori.nersc.gov:'+b+' \n rm -r ~/bin/port/* \n scp ~/bin/port/* jiuy97@cori.nersc.gov:'+b+' \n rm ~/bin/port/*', shell=True)
--- send-vasp.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-import sys
-
-a = input("to where?: ")
-subprocess.call('scp *.vasp hailey@134.79.69.172:~/Desktop/'+a+'', shell=True)
