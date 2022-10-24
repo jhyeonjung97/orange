e8c95e497e58871332abd6178387300ecf7c743c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-10-24 08:00:06 -0700

    .

--- jobname.sh
+++ jobname.sh
@@ -2,11 +2,11 @@
 
 read -p "job name: " a
 read -p "how many: " b
-if [[ -z "$b" ]]; then
-    sed -i -e "/job-name/c\#SBATCH --job-name="$a"" run_slurm.sh
+if [[ -z $b ]]; then
+    sed -i "/job-name/c\#SBATCH --job-name=""$a""" run_slurm.sh
 else
     for i in {0..$b}
     do
-    sed -i -e "/job-name/c\#SBATCH --job-name="$a$c"" $c*/run_slurm.sh
+    sed -i "/job-name/c\#SBATCH --job-name=""$a$c""" $c*/run_slurm.sh
     done
 fi
