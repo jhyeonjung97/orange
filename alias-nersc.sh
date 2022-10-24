db07a4643afe112d4ffebb82b1014e451531803a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-10-24 07:13:30 -0700

    .

--- alias-nersc.sh
+++ alias-nersc.sh
@@ -5,4 +5,4 @@ alias qdel='scancel'
 alias priority='squeue -o "%.10F %.10u %.20j %.2P %.5Q %.2t %.2Y" -S "t,-Q"'
 alias p='pestat -N'
 
-alias cdw='/global/cscratch1/sd/jiuy97'
+alias cdw='cd /global/cscratch1/sd/jiuy97'
