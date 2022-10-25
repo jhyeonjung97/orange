33b8d0ea47ba227bfcbb859c3113bc5985230a9e
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-10-25 13:05:03 -0700

    .

--- nebsend.sh
+++ nebsend.sh
@@ -2,15 +2,16 @@
 
 read -p "files starts with: " f
 
-a=$(($1+1))
-for i in $(seq 0 $a)
+for i in $(seq 1 $1)
 do
-    cp 0$i/POSCAR $a-p$i.vasp
+    cp 0$i/POSCAR $f-p$i.vasp
     cp 0$i/CONTCAR $f-c$i.vasp
 done
 
+cp 00/POSCAR $f-p0.vasp
+cp 0$(($1+1))/POSCAR $f-p$(($1+1)).vasp
 cp 00/POSCAR $f-c0.vasp
-cp 0$a/POSCAR $f-c$a.vasp
+cp 0$(($1+1))/POSCAR $f-c$(($1+1)).vasp
 
 read -p "to where?: " p
 
