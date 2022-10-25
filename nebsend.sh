fba57701ddced80d539b22e6a714dd652aebe0a8
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-10-25 12:53:38 -0700

    .

---
+++ nebsend.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p "how many images: " a
+read -p "files starts with: " f
+
+a=$a+1
+for i in $(seq 0 $a)
+do
+    cp 0$i/POSCAR $a-p$i.vasp
+    cp 0$i/CONTCAR $f-c$i.vasp
+done
+
+cp 00/POSCAR $f-c0.vasp
+cp 0$b/POSCAR $f-c$a.vasp
+
+read -p "to where?: " p
+
+echo "scp *.vasp hailey@134.79.69.172:~/Desktop/$p"
+scp *.vasp hailey@134.79.69.172:~/Desktop/$p
