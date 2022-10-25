85dfa2a0dbb4c1d583b1aa6892d330722acd7597
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-10-25 16:56:30 -0700

    .

--- send.sh
+++ send.sh
@@ -34,13 +34,13 @@ do
     
     # specific destination
     elif [[ $i == 'p' ]] || [[ $i == 'pos' ]]; then
-        file= $file + ' ' + 'POSCAR'
+        file=$file+' '+'POSCAR'
     elif [[ $i == 'c' ]] || [[ $i == 'con' ]]; then
-        file= $file + ' ' + 'CONTCAR'
+        file=$file+' '+'CONTCAR'
     elif [[ $i == 'port' ]]; then
-        file= $file + ' ' + '~/bin/port/*'
+        file=$file+' '+'~/bin/port/*'
     else 
-        file= $file + ' ' + $i
+        file=$file+' '+$i
     fi
 done
 
