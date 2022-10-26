2a883c8940d4957f4882e41c132217317ba6a10b
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-10-26 13:28:09 -0700

    .

--- .ipynb_checkpoints/repeat-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import sys
-
-a = int(sys.argv[1])
-b = int(sys.argv[2])
-c = input()
-for i in range(a,b+1):
-    d = str(i)
-    subprocess.call('cd '+d+' \n '+c+' \n cd ..', shell=True)
--- .ipynb_checkpoints/repeat-checkpoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -p "which command? " c
-
-for i in {$1..$2}
-do
-    $c
-done
