99a6d37b9d39abc05f98b60fc34ee5aaf0eed86e
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-10-26 01:18:50 -0700

    .

--- get.sh
+++ get.sh
@@ -38,7 +38,7 @@ do
         path='~/port'
         file='*'
     else 
-        file=$i
+        file="$i"
     fi
     
 done
@@ -58,3 +58,47 @@ fi
 
 echo "scp$port$r $srvr$path/$file ."
 scp$port$r $srvr$path/$file .
+The default interactive shell is now zsh.
+To update your account to use zsh, please run `chsh -s /bin/zsh`.
+For more details, please visit https://support.apple.com/kb/HT208050.
+(base) Haileys-MBP:~ hailey$ cori
+***************************************************************************
+                          NOTICE TO USERS
+
+Lawrence Berkeley National Laboratory operates this computer system under
+contract to the U.S. Department of Energy.  This computer system is the
+property of the United States Government and is for authorized use only.
+Users (authorized or unauthorized) have no explicit or implicit
+expectation of privacy.
+
+Any or all uses of this system and all files on this system may be
+intercepted, monitored, recorded, copied, audited, inspected, and disclosed
+to authorized site, Department of Energy, and law enforcement personnel,
+as well as authorized officials of other agencies, both domestic and foreign.
+By using this system, the user consents to such interception, monitoring,
+recording, copying, auditing, inspection, and disclosure at the discretion
+of authorized site or Department of Energy personnel.
+
+Unauthorized or improper use of this system may result in administrative
+disciplinary action and civil and criminal penalties. By continuing to use
+this system you indicate your awareness of and consent to these terms and
+conditions of use. LOG OFF IMMEDIATELY if you do not agree to the conditions
+stated in this warning.
+
+*****************************************************************************
+
+Login connection to host cori07 :
+
+(jiuy97@cori.nersc.gov) Password + OTP:
+Last login: Fri Oct 21 14:54:48 2022 from pc91622.slac.stanford.edu
+----------------------------- Contact Information ------------------------------
+
+NERSC Contacts                https://www.nersc.gov/about/contact-us/
+NERSC Status                  https://www.nersc.gov/live-status/motd/
+NERSC: 800-66-NERSC (USA)     510-486-8600 (outside continental USA)
+
+----------------- Current Status as of 2022-10-25 20:46 PDT --------------------
+
+Compute Resources:
+Cori:            Available.
+Perlmutter:      Unavailable. 10/24/22 15:00-10/26/22 9:00 PDT, Scheduled
