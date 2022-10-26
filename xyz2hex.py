427c3f2889ab34465536ad6b3bf61fbe014c8caa
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-10-25 19:09:11 -0700

    .

--- xyz2hex.py
+++ xyz2hex.py
@@ -9,19 +9,19 @@ if len(argv) < 8 or '-h' in argv:
 filename = argv[1]
 atoms = read(filename)
 
-a = float(argv[2])
-b = float(argv[3])
-c = float(argv[4])
+# a = float(argv[2])
+# b = float(argv[3])
+# c = float(argv[4])
 
-alpha = float(argv[5])
-beta  = float(argv[6])
-gamma = float(argv[7])
+# alpha = float(argv[5])
+# beta  = float(argv[6])
+# gamma = float(argv[7])
 
-atoms.set_cell([a, b, c, alpha, beta, gamma])
-#atoms.set_cell([9., 9., 20., 90., 90., 90.])
+#atoms.set_cell([a, b, c, alpha, beta, gamma])
+atoms.set_cell([8.92652, 8.92652, 30.00000, 90.0000, 90.0000, 120.0000])
 write(filename.replace('xyz', 'vasp'), atoms, format='vasp')
 #write('mixture.xyz'.replace('xyz', 'vasp'), atoms, format='vasp')
 
-os.system(f"~/bin/pyband/xcell.py -i {filename.replace('xyz', 'vasp')}")
-os.system(f"mv out*.vasp {filename.replace('xyz', 'vasp')}")
+# os.system(f"~/bin/pyband/xcell.py -i {filename.replace('xyz', 'vasp')}")
+# os.system(f"mv out*.vasp {filename.replace('xyz', 'vasp')}")
 
