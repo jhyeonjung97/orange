279e18039218250e5e71130c0bdde908e17f0e6f
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-10-26 03:21:56 -0700

    .

--- sub.sh
+++ sub.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 if [[ -z $1 ]]; then
-    sed -i "/NPAR/c\NPAR   = $npar" INCAR
+    sed -i "/NPAR/c\NPAR   = ${npar}" INCAR
     grep NPAR INCAR
     grep Selective POSCAR
     grep MAGMOM INCAR 
     sbatch run_slurm.sh
     cd ..
 else
-    for i in {a..b}
+    for i in {$a..$b}
     do
         cd $i*
-        sed -i "/NPAR/c\NPAR   = $npar" INCAR
+        sed -i "/NPAR/c\NPAR   = ${npar}" INCAR
         grep NPAR INCAR
         grep Selective POSCAR
         grep MAGMOM INCAR 
