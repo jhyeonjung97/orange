6f6dfbf0bded87bfa504f0f8e472b60129b8fc7f
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-10-25 18:56:43 -0700

    .

--- xyz2hex.py
+++ xyz2hex.py
@@ -1,5 +1,6 @@
 from ase.io import read, write
 from sys import argv, exit
+import os
 
 if len(argv) < 8 or '-h' in argv:
     print("usage: xyz2con.py *.xyz a b c alpha beta gamma\n")
@@ -20,3 +21,7 @@ atoms.set_cell([a, b, c, alpha, beta, gamma])
 #atoms.set_cell([9., 9., 20., 90., 90., 90.])
 write(filename.replace('xyz', 'vasp'), atoms, format='vasp')
 #write('mixture.xyz'.replace('xyz', 'vasp'), atoms, format='vasp')
+
+os.system(f"~/bin/pyband/xcell.py {filename.replace('xyz', 'vasp')}")
+os.system(f"mv out*.vasp {filename.replace('xyz', 'vasp')}")
+
