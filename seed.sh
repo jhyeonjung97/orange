ec126115b5d1cfbd97833bad9e9fb9462cbd0208
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-10-28 17:53:35 -0700

    .

--- seed.sh
+++ seed.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-filename="${1%.*}"
-extension="${1##*.}"
+if [[ -z $2 ]]; then
+    echo 'usage: seed [FILENAME.inp] [SEED]'
+    break
+else
+    filename="${1%.*}"
+    extension="${1##*.}"
 
-sed "/seed/c\seed $2" $1 > $filename$2.inp
+    sed "/seed/c\seed $2" $1 > $filename$2.inp
+fi
