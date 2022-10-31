93e9b9d03f7574b41bbdf9be71fcc21ac08ae325
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-10-30 21:27:48 -0700

    .

--- autosub.sh
+++ autosub.sh
@@ -14,27 +14,26 @@ elif [[ ! -e "run_slurm.sh" ]]; then
     break
 elif [[ -z $2 ]]; then
     echo 'usage: autosub [directory#1] [directory#2]'
-    break
+else
+    read -p "POSCARs starts with: " p
+    read -p "job name: " n
+    
+    for i in $(seq $1 $2)
+    do
+        mkdir $i
+        cp INCAR KPOINTS run_slurm.sh $i
+        cp $p$i.vasp $i/POSCAR
+        cd $i
+        python ~/bin/pyband/xcell.py
+        mv out*.vasp POSCAR
+        sed -n 6p POSCAR >> temp1
+        sed 1d POSCAR >> temp2
+        cat temp1 temp2 > POSCAR
+        rm temp1 temp2
+        python3 ~/bin/orange/magmom.py
+        python3 ~/bin/shoulder/potcar_ara.py
+        sed -i "/job-name/c\#SBATCH --job-name=\"$n$i\"" run_slurm.sh
+        sh ~/bin/orange/sub.sh
+        cd ..
+    done
 fi
-
-read -p "POSCARs starts with: " p
-read -p "job name: " n
-
-for i in $(seq $1 $2)
-do
-    mkdir $i
-    cp INCAR KPOINTS run_slurm.sh $i
-    cp $p$i.vasp $i/POSCAR
-    cd $i
-    python ~/bin/pyband/xcell.py
-    mv out*.vasp POSCAR
-    sed -n 6p POSCAR >> temp1
-    sed 1d POSCAR >> temp2
-    cat temp1 temp2 > POSCAR
-    rm temp1 temp2
-    python3 ~/bin/orange/magmom.py
-    python3 ~/bin/shoulder/potcar_ara.py
-    sed -i "/job-name/c\#SBATCH --job-name=\"$n$i\"" run_slurm.sh
-    sh ~/bin/orange/sub.sh
-    cd ..
-done
