ef8fb7d4b9077073ed853cee7b8b0c1c1e99a5cc
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-10-31 13:33:38 -0700

    .

--- get.sh
+++ get.sh
@@ -20,7 +20,7 @@ do
     # specific destination server
     elif [[ $i == 'mac' ]]; then
         port=''
-        srvr='hailey@134.79.69.172:~/Desktop'
+        srvr='hailey@134.79.69.172:~/Desktop/'
     elif [[ $i == 'burning' ]]; then
         port='-P 1234 '
         srvr='hyeonjung@burning.postech.ac.kr:'
