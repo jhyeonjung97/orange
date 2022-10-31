f588b19806fd2ddf4d6b9039f2c775900c30685d
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-10-31 14:23:51 -0700

    .

--- jobname.sh
+++ jobname.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 if [[ -z $2 ]]; then
-    sed -i "/job-name/c\#SBATCH --job-name=\"$1\"" run_slurm.sh
+    sed -i "/#SBATCH --job-name/c\#SBATCH --job-name=\"$1\"" run_slurm.sh
+    sed -i "/#PBS -N/c\#PBS -N $1" run_slurm.sh
 elif [[ $1 == '-r' ]]; then
     for i in {0..9}
     do
     if [[ -e $i ]]; then
-        sed -i "/job-name/c\#SBATCH --job-name=\"$2$i\"" $i*/run_slurm.sh
+        sed -i "/#SBATCH --job-name/c\#SBATCH --job-name=\"$2$i\"" $i*/run_slurm.sh
+        sed -i "/#PBS -N/c\#PBS -N $2$i" run_slurm.sh
     fi
     done
 fi
