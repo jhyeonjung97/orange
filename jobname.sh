bae12cc02b5808e720d129d0f9e1e692e475c4cd
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-11-01 10:54:40 -0700

    .

--- jobname.sh
+++ jobname.sh
@@ -1,38 +1,51 @@
 #!/bin/bash
 
-if [[ $1 =~ '-h' ]] || [[ $1 =~ '--h' ]] || [[ -z $1 ]]; then
+function usage_error{
     echo 'usage: name (-r) [jobname] (dir#1) (dir#2)'
     exit 1
+}
+
+if [[ $1 =~ '-h' ]] || [[ $1 =~ '--h' ]] || [[ -z $1 ]]; then
+    usage_error
 fi
 
-if [[ $1 == '-r' ]]; then
-
-    if [[ -z $2 ]]; then
-        echo 'usage: name (-r) [jobname] (dir#1) (dir#2)'
-        exit 1
-    elif [[ -z $3 ]]; then
-        a=0; b=9
-    elif [[ -z $4 ]]; then
-        a=1; b=$3
-    elif [[ -z $5 ]]; then
-        a=$3; b=$4
+function numb {
+    if [ $1 -eq $1 ] 2>/dev/null ; then
+        return 0
     else
-        echo 'usage: name (-r) [jobname] (dir#1) (dir#2)'
-        exit 1
+        usage_error
     fi
-    
-    for i in $(seq $a $b)
-    do
-    sed -i "/#SBATCH --job-name/c\#SBATCH --job-name=\"$2$i\"" $i*/run_slurm.sh
-    sed -i "/#PBS -N/c\#PBS -N $2$i" $i*/run_slurm.sh
-    done
-    
-elif [[ -z $2 ]]; then
+}
+
+if [[ -z $2 ]]; then
     sed -i "/#SBATCH --job-name/c\#SBATCH --job-name=\"$1\"" run_slurm.sh
     sed -i "/#PBS -N/c\#PBS -N $1" run_slurm.sh
+    exit 0
     
+elif [[ $1 == '-r' ]] ; then
+    if [[ -z $2 ]]; then
+        usage_error
+    elif [[ -z $3 ]]; then
+        name=$2; a=0; b=9
+    elif [[ -z $4 ]]; then
+        name=$2; a=1; b=$3
+    elif [[ -z $5 ]]; then
+        name=$2; a=$3; b=$4
+    else
+        usage_error
+    fi
+
+elif [[ numb $2 ]] && [[ -z $3 ]]; then
+    name=$2; a=1; b=$3
+elif [[ numb $3 ]] && [[ -z $4 ]]; then
+    name=$2; a=$3; b=$4
 else
-    echo 'usage: name (-r) [jobname] (number)'
-    exit 1
+    usage_error
 fi
 
+# loop
+for i in $(seq $a $b)
+do
+sed -i "/#SBATCH --job-name/c\#SBATCH --job-name=\"$name$i\"" $i*/run_slurm.sh
+sed -i "/#PBS -N/c\#PBS -N $name$i" $i*/run_slurm.sh
+done
