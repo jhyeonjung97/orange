4c4c75f3726ba060b0496ad3e0846a953b125e8b
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-11-02 16:53:41 -0700

    .

--- alias-basic.sh
+++ alias-basic.sh
@@ -130,5 +130,6 @@ sub'
 
 #Electronic structure
 alias aloha='python3 ~/bin/orange/cohp.py'
+alias charge='python3 ~/bin/orange/charge.py'
 alias bader='chgsum.pl AECCAR0 AECCAR2
 bader CHGCAR -ref CHGCAR_sum'
