b9a1ac920ca620eb8ad507f764874904fa8e9f97
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-11-02 16:59:06 -0700

    .

--- charge.py
+++ charge.py
@@ -46,7 +46,7 @@ def attach_charges(atoms, fileobj='ACF.dat', element='O'):
         if atom.symbol == element:
             print(f"{element}{atom.index}: \t {atom.charge}")
             total+=atom.charge
-    print(f"{element}_total: \t {total}")
+    print(f"{element}_tot: \t {total}")
 
 atoms = read('POSCAR')
 fileobj = 'ACF.dat'
