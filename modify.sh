ae3db467b1d03f887485d1aca8a17e2e1bfebc59
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-11-02 18:31:52 -0700

    .

--- modify.sh
+++ modify.sh
@@ -12,29 +12,5 @@ function modify {
     fi
 }
 
-#prepare input files
-if [[ $1 == 'chg' ]]; then
-    cp INCAR INCAR_chg
-    echo '<INCAR_chg>'
-    modify INCAR_chg NSW
-    modify INCAR_chg IBRION
-    modify INCAR_chg LCHARG
-    modify INCAR_chg LAECHG .TRUE.
-    modify INCAR_chg LORBIT
-fi
-
-if [[ $1 == 'dos' ]]; then
-    cp INCAR INCAR_dos
-    echo '<INCAR_dos>'
-    modify INCAR_dos ICHARG 11
-    modify INCAR_dos NSW
-    modify INCAR_dos IBRION
-    modify INCAR_dos ISMEAR -5
-    modify INCAR_dos ALGO
-    modify INCAR_dos LCHARG .FALSE.
-    modify INCAR_dos LAECHG
-    modify INCAR_dos LORBIT 11
-    modify INCAR_dos NEDOS 1000
-    modify INCAR_dos EMIN -50
-    modify INCAR_dos EMAX 50
-fi
+modify $1 $2 $3
+grep $2 $1
