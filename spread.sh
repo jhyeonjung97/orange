a316d51e3a6aef58c9473f02d0047b77ac0f53dd
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-11-03 16:38:15 -0700

    .

--- spread.sh
+++ spread.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-if [[ -z $2 ]]; then
-    SET='*/'
-elif [[ $2 == '2' ]]; then
-    SET='*/*/'
-elif [[ $2 == '3' ]]; then
-    SET='*/*/*/'
+SET='*/'
+file=$1
+
+if [[ -n $2 ]]; then
+    file=$2
+    for i in $((seq 2 $1)); do
+        SET+='*/'
+    done
 fi
 
-for dir in $SET
-do
-    cp $1 $dir
+for dir in $SET; do
+    cp $file $dir
 done
