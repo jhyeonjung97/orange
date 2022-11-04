aa80e8db7c5fb1aba13b05d906a4f7e65ba56c92
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-11-03 17:40:34 -0700

    .

--- resub.sh
+++ resub.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-rm STD* std* *.e* *.o*
-sh ~/bin/orange/sub.sh
+function resub {
+    if [[ ${here} == 'nurion' ]] || [[ ${here} == 'kisti' ]]; then
+        rm *.e* *.o*
+    else
+        rm STD* std*
+    fi
+    sh ~/bin/orange/sub.sh
+}
+
+if [[ $1 == '-r' ]]
+    for dir in */
+    do
+        cd $dir
+        resub
+        cd ..
+    done
+else
+    resub
+fi
