d59a79293001427d4f935410541ae2b434fe040c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-11-03 17:31:33 -0700

    .

--- jobname.sh
+++ jobname.sh
@@ -48,8 +48,9 @@ fi
 # loop
 for i in $SET
 do
-sed -i "/#SBATCH --job-name/c\#SBATCH --job-name=\"$name$i\"" $i$star/run_slurm.sh
-sed -i "/#PBS -N/c\#PBS -N $name$i" $i$star/run_slurm.sh
+    i=${i#/}
+    sed -i "/#SBATCH --job-name/c\#SBATCH --job-name=\"$name$i\"" $i$star/run_slurm.sh
+    sed -i "/#PBS -N/c\#PBS -N $name$i" $i$star/run_slurm.sh
 done
 
 grep $name */run_slurm.sh
