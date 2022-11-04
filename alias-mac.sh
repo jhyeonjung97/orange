7407718f237286d0a0087b500b0519ff79d06c04
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-11-03 17:51:47 -0700

    .

--- alias-mac.sh
+++ alias-mac.sh
@@ -7,7 +7,7 @@ alias cdw='cd ~/Desktop/'
 alias vbash='vi ~/.zshrc'
 alias sbash='source ~/.zshrc'
 alias vasp2png='python ~/bin/shoulder/vasp2png.py'
-alias element='vi /Applications/VESTA/VESTA.app/Contents/Resources/elements.ini'
+alias element='open /Applications/VESTA/VESTA.app/Contents/Resources/elements.ini'
 
 alias orange='dir_now=$PWD
 cd ~/bin/orange
