59ead3764dea01becc42169217b8e6edf6cd22f6
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-11-17 04:37:03 -0800

    .

--- cell.sh
+++ cell.sh
@@ -4,7 +4,10 @@ do
     cif2cell $file -p quantum-espresso -o $filename.in
     grep nat $filename.in > $filename.data
     grep ntyp $filename.in >> $filename.data
-    grep PSEUDO $filename.in >> $filename.data
-    sed -i '1,/ATOMIC_POSITIONS {crystal}/d' $filename.in
-    sed -i '1i\ATOMIC_POSITIONS {crystal}' $filename.in
+    sed -i '1,/ATOMIC_SPECIES/d' $filename.in
+    sed -i '1i\ATOMIC_SPECIES' $filename.in
+    
+    sed -i 's/H_PSEUDO/H.pbe-kjpaw_psl.1.0.0.UPF/' $filename.in
+    sed -i 's/O_PSEUDO/O.pbe-nl-kjpaw_psl.1.0.0.UPF/' $filename.in
+    sed -i 's/Li_PSEUDO/Li.pbe-sl-kjpaw_psl.1.0.0.UPF/' $filename.in
 done
