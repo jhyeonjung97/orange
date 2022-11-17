b5dc410ed0996fe36cdf7389f9be29fb618deb31
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-11-17 04:20:43 -0800

    .

--- cell.sh
+++ cell.sh
@@ -2,6 +2,9 @@ for file in *.cif
 do
     filename="${file%.*}"
     cif2cell $file -p quantum-espresso -o $filename.in
-    sed -n 10,23p $filename.in > $filename.data
-    sed -i 1,23d $filename.in
+    grep nat $filename.in > $filename.data
+    grep ntyp $filename.in > $filename.data
+    grep PSEUDO $filename.in > $filename.data
+    sed -i '1,/ATOMIC_POSITIONS {crystal}/d' $filename.in
+    sed -i '1i\ATOMIC_POSITIONS {crystal}' $filename.in
 done
