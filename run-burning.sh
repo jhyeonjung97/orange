5d3e122433ec74c0566c4df4971b0912818fa7f2
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-11-17 04:45:14 -0800

    .

--- run-burning.sh
+++ run-burning.sh
@@ -50,7 +50,7 @@ function in_array {
 }
 
 if in_array "qe" "${type[*]}"; then
-    sed -e '/\/TGM\/Apps\/VASP\/VASP_BIN\/6.3.2\/vasp.6.3.2.std.x/c\pw.x -in qe-relax.in' run_slurm.sh
+    sed -i '/\/TGM\/Apps\/VASP\/VASP_BIN\/6.3.2\/vasp.6.3.2.std.x/c\pw.x -in qe-relax.in' run_slurm.sh
     sed -i 'cat incar.in poscar.in kpoints.in > qe-relax.in' run_slurm.sh
 fi
 
