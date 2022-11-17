584acf21ec94cfaea30cdd864e1327d5ce6d207e
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-11-17 04:22:34 -0800

    .

--- cell.sh
+++ cell.sh
@@ -3,8 +3,8 @@ do
     filename="${file%.*}"
     cif2cell $file -p quantum-espresso -o $filename.in
     grep nat $filename.in > $filename.data
-    grep ntyp $filename.in > $filename.data
-    grep PSEUDO $filename.in > $filename.data
+    grep ntyp $filename.in >> $filename.data
+    grep PSEUDO $filename.in >> $filename.data
     sed -i '1,/ATOMIC_POSITIONS {crystal}/d' $filename.in
     sed -i '1i\ATOMIC_POSITIONS {crystal}' $filename.in
 done
