57e20c03bb001a042590759e0b73fba2a5cb666b
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-11-17 02:40:59 -0800

    .

--- relax.sh
+++ relax.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 cat incar.in potcar.in $1.in kpoints.in > qe-relax.in
-grep nat incar.in
 grep nat $1.data
-grep ntyp incar.in
+grep nat incar.in
 grep ntyp $1.data
+grep ntyp incar.in
+sed -n '/ATOMIC_SPECIES/,$p' $1.data
+sed -n '/ATOMIC_SPECIES/,$p' potcar.in
