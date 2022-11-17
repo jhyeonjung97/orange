3cf5b620721ba6afe55babda6c86012f30f99e64
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-11-17 01:19:06 -0800

    .

--- cell.sh
+++ cell.sh
@@ -2,4 +2,6 @@ for file in *.cif
 do
     filename="${file%.*}"
     cif2cell $file -p quantum-espresso -o $filename.in
+    sed -n 10,23p $filename.in > $filename.data
+    sed 1,23d $filename.in
 done
