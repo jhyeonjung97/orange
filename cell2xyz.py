9cfcbccff06f26b963d3833f997d3c4f1830bd8b
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-11-22 02:14:43 -0800

    .

--- cell2xyz.py
+++ cell2xyz.py
@@ -2,15 +2,16 @@ from ase.io import read, write
 from sys import argv, exit
 import os
 
-# usage: cell2xyz.py [file] [lattice_a] [format]
-file = str(argv[1])
-a = float(argv[2])
-# format = argv[3]
+# usage: cell2xyz.py [file_in] [file_out] [lattice_a] [format]
+file_in = str(argv[1])
+file_out = str(argv[2])
+a = float(argv[3])
+# format = argv[4]
 
-atoms = read(file)
+atoms = read(file_in)
 atoms.positions *= a
 # atoms.set_cell([a, a, a])
 
-write('contcar.xyz', atoms, format='xyz')
+write(file_out, atoms, format='xyz')
 # if not format == '';
 #     write('contcar.%s' %format, atoms, format=format)
