258ea5630c1f7f4862c07b005b8759a6ccb72e7a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-11-22 02:11:39 -0800

    .

--- out2xyz.sh
+++ out2xyz.sh
@@ -31,3 +31,15 @@ echo $atoms >> .contcar.xyz
 a=40
 python ~/bin/orange/cell2xyz.py .contcar.xyz $a
 sed -i -e "1a$nat" -e '2d' contcar.xyz
+
+atoms=$(grep ATOMIC_POSITIONS qe-relax.in -A $nat | tail -n $nat )
+
+if [[ -e .poscar.xyz ]]; then
+    rm .poscar.xyz
+fi
+echo $nat >> .poscar.xyz
+echo $PWD >> .poscar.xyz
+echo $atoms >> .poscar.xyz
+
+python ~/bin/orange/cell2xyz.py .poscar.xyz $a
+sed -i -e "1a$nat" -e '2d' poscar.xyz
