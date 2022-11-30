9c04d447a08d0569c89a7857dc1f6b8e2c2e928c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-11-29 18:53:34 -0800

    .

---
+++ ta.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $1 =~ q ]]; then
+    for dir in */
+    do
+        cd $dir
+        echo "==> $dir/stdout.log <=="
+        grep '!    total energy' stdout.log | tail -n 1
+        cd ..
+    done
+else
+    tail -n 6 */stdout*
+fi
