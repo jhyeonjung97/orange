a27f1f03bb75a3079fe77bc09cab50cd70891122
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-11-29 18:56:30 -0800

    .

--- ta.sh
+++ ta.sh
@@ -4,8 +4,7 @@ if [[ $1 =~ q ]]; then
     for dir in */
     do
         cd $dir
-        echo "$dir"stdout.log
-        grep '!    total energy' stdout.log | tail -n 1
+        echo [$dir] $(grep '!    total energy' stdout.log | tail -n 1)
         cd ..
     done
 else
