4d22ca7f786e5007f9b4413f5240bbd8b206bd19
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-12-04 03:33:19 -0800

    .

--- alias-kisti.sh
+++ alias-kisti.sh
@@ -6,5 +6,5 @@ alias pestat='pbs_status | head -n 2
 pbs_status | grep normal
 pbs_status | grep norm_skl
 pbs_status | grep long'
-alias mystat='qstat -u x2431a10
+alias mystat='qstat -u x2347a10
 qstat -u x2431a10'
