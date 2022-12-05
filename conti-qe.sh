24114d0587be1631a133d0906450cd8810bf388f
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-12-04 22:26:03 -0800

    .

--- conti-qe.sh
+++ conti-qe.sh
@@ -1,35 +1,6 @@
 #!/bin/bash
 
-function conti {
-    i=1
-    save="conti_$i"
-    while [[ -d "conti_$i" ]]
-    do
-        i=$(($i+1))
-        save="conti_$i"
-    done
-    sh out2xyz.sh
-    mkdir $save
-    mv * $save
-    cd $save/
-    mv */ ..
-    cp poscar.in ../initial.in
-    cp poscar.in contcar.in incar.in kpoints.in potcar.in run_slurm.sh initial.in ..
-    cd ..
 
-    if [[ -s contcar.in ]]; then
-        mv contcar.in poscar.in
-    fi
-
-    if [[ ${here} == 'burning' ]]; then
-        sbatch run_slurm.sh
-    elif [[ ${here} == 'nurion' ]] || [[ ${here} == 'kisti' ]]; then
-        qsub run_slurm.sh
-    else
-        echo 'where am i..? please modify [conti-qe.sh] code'
-        exit 1
-    fi
-}
 
 if [[ -z $1 ]]; then # simple conti
     conti
--- conti.sh
+++ conti.sh
@@ -30,13 +30,48 @@ function conti {
     fi
 }
 
+function conti-qe {
+    i=1
+    save="conti_$i"
+    while [[ -d "conti_$i" ]]
+    do
+        i=$(($i+1))
+        save="conti_$i"
+    done
+    sh out2xyz.sh
+    mkdir $save
+    mv * $save
+    cd $save/
+    mv */ ..
+    cp poscar.in ../initial.in
+    cp poscar.in contcar.in incar.in kpoints.in potcar.in run_slurm.sh initial.in ..
+    cd ..
+
+    if [[ -s contcar.in ]]; then
+        mv contcar.in poscar.in
+    fi
+
+    if [[ ${here} == 'burning' ]]; then
+        sbatch run_slurm.sh
+    elif [[ ${here} == 'nurion' ]] || [[ ${here} == 'kisti' ]]; then
+        qsub run_slurm.sh
+    else
+        echo 'where am i..? please modify [conti-qe.sh] code'
+        exit 1
+    fi
+}
+
 if [[ $1 == '-qe' ]]; then
     sh ~/bin/conti-qe.sh ${@:2}
     exit 1
 fi
 
 if [[ -z $1 ]]; then # simple conti
-    conti
+    if [[ -z $(grep pw.x run_slurm.sh) ]]; then
+        conti
+    else
+        conti-qe
+    fi
 else
     if [[ $1 == '-r' ]] || [[ $1 == 'all' ]]; then
         DIR='*/'
@@ -52,7 +87,11 @@ else
     do
         i=${i%/}
         cd $i*
-        conti
+        if [[ -z $(grep pw.x run_slurm.sh) ]]; then
+            conti
+        else
+            conti-qe
+        fi
         cd ..
     done
 fi
