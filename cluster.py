62459d07ec17a1e62ecf44bf344b1e12d06064fc
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-12-04 21:30:14 -0800

    .

---
+++ cluster.py
@@ -0,0 +1,22 @@
+from ase.io import read, write
+from sys import argv
+import os
+
+if not argv[2]:
+    print('wrong usage...')
+    exit()
+
+name, ext = os.path.splitext(argv[2])
+
+a == argv[3]
+if a == '':
+    a = input("lattice parameter? ")
+if a == '':
+    print('use default lattice parameter, 30 A ...')
+    a = 30.
+
+# iterating over all files
+atoms = read(argv[1])
+atoms.set_cell([a, a, a])
+atoms.center()
+write(argv[2], atoms, format=ext)
