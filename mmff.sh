8ef39595d7d36a1b7ddc969744e254da0676ecfa
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-12-04 23:57:08 -0800

    .

--- mmff.sh
+++ mmff.sh
@@ -15,8 +15,11 @@ if [[ -z $a ]]; then
 fi
 
 if [[ -f $name.$ext ]]; then
-    python ~/bin/orange/cluster.py $name.$ext $name.xyz $a
-    obabel $name.xyz -O $name.mol2
+    # python ~/bin/orange/cluster.py $name.$ext $name.xyz $a
+    # obabel $name.xyz -O $name.mol2
+    if [[ $ext != 'mol2' ]]; then
+        obabel $name.$ext -O $name.mol2
+    fi
     obminimize -n 100000 -sd -c 1e-10 -ff MMFF94s $name.mol2 > $name.pdb
     python ~/bin/orange/cluster.py $name.pdb $name.xyz $a
 fi
@@ -24,10 +27,11 @@ fi
 for i in {0..9}
 do
     if [[ -f $name$i.$ext ]]; then
-        python ~/bin/orange/cluster.py $name$i.$ext $name$i.cif $a
-        # echo "python ~/bin/orange/cluster.py $name$i.$ext $name$i.cif $a"
-        obabel $name$i.cif -O $name$i.mol2
-        # echo "obabel $name$i.cif -O $name$i.mol2"
+        # python ~/bin/orange/cluster.py $name$i.$ext $name$i.xyz $a
+        # obabel $name$i.xyz -O $name$i.mol2
+        if [[ $ext != 'mol2' ]]; then
+            obabel $name$i.$ext -O $name$i.mol2
+        fi
         obminimize -n 100000 -sd -c 1e-10 -ff MMFF94s $name$i.mol2 > $name$i.pdb
         python ~/bin/orange/cluster.py $name$i.pdb $name$i.xyz $a
     fi
