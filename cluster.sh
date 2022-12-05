1ebaba75fb88dafb816dc64567e944a98a7b9286
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-12-04 21:59:39 -0800

    .

--- cluster.sh
+++ cluster.sh
@@ -3,8 +3,12 @@
 if [[ -z $1 ]] || [[ -z $2 ]]; then
     echo 'wrong usage...'
     exit 1
-elif [[ -z $3 ]]; then
-    read -p 'lattice parameter? (A) ' 3
+else
+    a=$3
+fi
+
+if [[ -z $a ]]; then
+    read -p 'lattice parameter? (A) ' a
 fi
 
 name1="${1%.*}"
@@ -14,13 +18,13 @@ name2="${2%.*}"
 ext2="${2##*.}"
 
 if [[ -e $1 ]]; then
-    python ~/bin/orange/cluster.py $1 $2 $3
+    python ~/bin/orange/cluster.py $1 $2 $a
 fi
     
 for i in {0..9}
 do
     if [[ -e $name1$i.$ext1 ]]; then
-        python ~/bin/orange/cluster.py $name1$i.$ext1 $name2$i.$ext2 $3
-        # echo "python ~/bin/orange/cluster.py $name1$i.$ext1 $name2$i.$ext2 $3"
+        python ~/bin/orange/cluster.py $name1$i.$ext1 $name2$i.$ext2 $a
+        # echo "python ~/bin/orange/cluster.py $name1$i.$ext1 $name2$i.$ext2 $a"
     fi
 done
--- mmff.sh
+++ mmff.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
-extension="${1##*.}"
-filename="${1%.*}"
+name="${1%.*}"
+ext="${1##*.}"
 
-if [[ -f $1 ]]; then
-    obminimize -n 100000 -sd -c 1e-10 -ff MMFF94s $1 > $filename.pdb
+a=$2
+
+if [[ -z $a ]]; then
+    read -p 'lattice parameter? (A) ' a
+fi
+
+if [[ -z $a ]]; then
+    echo 'use default lattice parameter 30 A...'
+    a=30.
+fi
+
+if [[ -f $name.$ext ]]; then
+    python ~/bin/orange/cluster.py $name.$ext $name.xyz $a
+    obabel $name.xyz -O $name.mol2
+    obminimize -n 100000 -sd -c 1e-10 -ff MMFF94s $name.mol2 > $name.pdb
+    python ~/bin/orange/cluster.py $name.pdb $name.xyz $a
 fi
     
 for i in {0..9}
 do
-    if [[ -f $filename$i.mol2 ]]; then
-        obminimize -n 100000 -sd -c 1e-10 -ff MMFF94s $filename$i.mol2 > $filename$i.pdb
+    if [[ -f $name$i.$ext ]]; then
+        python ~/bin/orange/cluster.py $name$i.$ext $name$i.xyz $a
+        echo "python ~/bin/orange/cluster.py $name$i.$ext $name$i.xyz $a"
+        obabel $name$i.xyz -O $name$i.mol2
+        echo "obabel $name$i.xyz -O $name$i.mol2"
+        obminimize -n 100000 -sd -c 1e-10 -ff MMFF94s $name$i.mol2 > $name$i.pdb
+        python ~/bin/orange/cluster.py $name$i.pdb $name$i.xyz $a
     fi
 done
