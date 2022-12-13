5655f3f9fdab3c59ee9b97dc6a92736ff5e0163c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-12-13 09:41:09 -0800

    .

--- nelect.sh
+++ nelect.sh
@@ -2,13 +2,12 @@ nchg_tag=$(grep NETCHG INCAR | sed 's/\t/ /g')
 IFS=' '
 read -ra nchg_arr <<< $nchg_tag
 nchg=${nchg_arr[2]}
-echo $nchg
+sed -i -e '/NETCHG/d' INCAR
 
 ntyp_tag=$(sed -n 6p POSCAR | sed 's/\t/ /g')
 IFS=' '
 read -ra ntyp_arr <<< $ntyp_tag
 ntyp=${#ntyp_arr[@]}
-# ntyp=${ntyp_arr[2]}
 
 nat_tag=$(sed -n 7p POSCAR | sed 's/\t/ /g')
 IFS=' '
@@ -28,4 +27,4 @@ done
 
 nelect0=$(echo "$nelect $nchg" | awk '{print $1 + $2}')
 sh ~/bin/orange/modify.sh INCAR NELECT $nelect0
-echo "NELECT_neutral: $nelect, NELECT: $nelect0, net_charge: $nchg"
+echo "NELECT_neutral: $nelect, NELECT: $nelect0 (net_charge: $nchg)"
