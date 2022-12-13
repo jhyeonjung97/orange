2351e48673f88dc33b130f3d6bb26282afbcf195
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-12-13 09:10:09 -0800

    .

--- nelect.sh
+++ nelect.sh
@@ -8,16 +8,19 @@ nat_tag=$(sed -n 7p POSCAR | sed 's/\t/ /g')
 IFS=' '
 read -ra nat_arr <<< $nat_tag
     
+step=1
+nelect=0
 for i in $(seq 1 $ntyp)
 do
-    typ=${ntyp_arr[`expr $i + 1`]}
-    nat=${nat_arr[`expr $i + 1`]}
-    zval_tag=$(grep ZVAL POTCAR | sed 's/\t/ /g' | sed -n "$i{p}")
+    j=$(echo "$i $step" | awk '{print $1 - $2}')
+    typ=${ntyp_arr[$j]}
+    nat=${nat_arr[$j]}
+    zval_tag=$(grep ZVAL POTCAR | sed 's/\t/ /g' | sed -n "$i"p)
     IFS=' '
     read -ra zval_arr <<< $zval_tag
     zval=${zval_arr[5]}
-    nelect+=$(echo "$nat $zval" | awk '{print $1 * $2}')
-    echo $i $j $typ $nat $zval $nelect
+    nelect=$(echo "$nelect $nat $zval" | awk '{print $1 + ($2 * $3)}')
+    echo $i $typ $nat $zval $nelect
 done
 
 sh ~/bin/orange/modify.sh NELECT $nelect
