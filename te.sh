73591e13d081d1350d95ce648839c1479492b3b0
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-12-24 09:58:26 -0800

    .

--- te.sh
+++ te.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+function update {
+    force_tag=$(grep 'Total force' $1 | tail -n 1 | sed 's/\t/ /')
+    read -ra force_arr <<< $force_tag
+    force=${force_arr[3]}
+    energy_tag=$(grep 'total energy' $1 | grep Ry | tail -n 1 | sed 's/\t/ /')
+    read -ra energy_arr <<< $energy_tag
+    if [[ ${energy_arr[3]} =~ '=' ]]; then
+        energy=${energy_arr[4]}
+    else
+        energy=${energy_arr[3]}
+    fi
+}
+
 IFS=' '
 if [[ $1 =~ qe ]] || [[ -n $(grep pw.x run_slurm.sh) ]]; then
     echo 'Dir   Total force   Total energy   Status  '
@@ -8,31 +21,21 @@ if [[ $1 =~ qe ]] || [[ -n $(grep pw.x run_slurm.sh) ]]; then
     do
         cd $dir
         if [[ -e stdout.log ]]; then
-            force_tag=$(grep 'Total force' stdout.log | tail -n 1 | sed 's/\t/ /')
-            read -ra force_arr <<< $force_tag
-            force=${force_arr[3]}
-            energy_tag=$(grep 'total energy' stdout.log | grep Ry | tail -n 1 | sed 's/\t/ /')
-            read -ra energy_arr <<< $energy_tag
-            energy=${energy_arr[3]}
             if [[ -n $(grep 'JOB DONE' stdout.log) ]]; then
                 stat='DONE'
             else
                 stat='Not DONE'
             fi
+            update stdout.log
         elif [[ -f conti_1/stdout.log ]]; then
             i=1
+            stat='Continued'
             while [[ -f "conti_$i/stdout.log" ]]
             do
                 conti="conti_$i"
                 i=$(($i+1))
             done
-            force_tag=$(grep 'Total force' $conti/stdout.log | tail -n 1 | sed 's/\t/ /')
-            read -ra force_arr <<< $force_tag
-            force=${force_arr[3]}
-            energy_tag=$(grep 'total energy' $conti/stdout.log | grep Ry | tail -n 1 | sed 's/\t/ /')
-            read -ra energy_arr <<< $energy_tag
-            energy=${energy_arr[3]}
-            stat='Continued'
+            update $conti/stdout.log
         else
             force=''
             energy=''
