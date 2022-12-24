3c1042f2adf6c4e4afb4eb6f1ade5487a5f9b01a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-12-24 09:44:34 -0800

    .

--- te.sh
+++ te.sh
@@ -13,10 +13,11 @@ if [[ $1 =~ qe ]] || [[ -n $(grep pw.x run_slurm.sh) ]]; then
         energy_tag=$(grep 'total energy' stdout.log | grep Ry | tail -n 1 | sed 's/\t/ /')
         read -ra energy_arr <<< $energy_tag
         energy=${energy_arr[3]}
-        if [[ -e stdout.log ]] && [[ -n $(grep 'JOB DONE' stdout.log) ]]; then
-            stat='DONE'
-        else
-            stat='Not DONE'
+        stat='Not DONE'
+        if [[ -e stdout.log ]]; then
+            if [[ -n $(grep 'JOB DONE' stdout.log) ]]; then
+                stat='DONE'
+            fi
         fi
         printf '%-5s %-13s %-14s %-8s\n' $dir $force $energy $stat
         cd ..
