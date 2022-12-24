d7152989f2176fea22721827ff9548b16b9f48b0
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-12-24 09:54:12 -0800

    .

--- te.sh
+++ te.sh
@@ -7,17 +7,36 @@ if [[ $1 =~ qe ]] || [[ -n $(grep pw.x run_slurm.sh) ]]; then
     for dir in */
     do
         cd $dir
-        force_tag=$(grep 'Total force' stdout.log | tail -n 1 | sed 's/\t/ /')
-        read -ra force_arr <<< $force_tag
-        force=${force_arr[3]}
-        energy_tag=$(grep 'total energy' stdout.log | grep Ry | tail -n 1 | sed 's/\t/ /')
-        read -ra energy_arr <<< $energy_tag
-        energy=${energy_arr[3]}
-        stat='Not DONE'
         if [[ -e stdout.log ]]; then
+            force_tag=$(grep 'Total force' stdout.log | tail -n 1 | sed 's/\t/ /')
+            read -ra force_arr <<< $force_tag
+            force=${force_arr[3]}
+            energy_tag=$(grep 'total energy' stdout.log | grep Ry | tail -n 1 | sed 's/\t/ /')
+            read -ra energy_arr <<< $energy_tag
+            energy=${energy_arr[3]}
             if [[ -n $(grep 'JOB DONE' stdout.log) ]]; then
                 stat='DONE'
+            else
+                stat='Not DONE'
             fi
+        elif [[ -f conti_1/stdout.log ]]; then
+            i=1
+            while [[ -f "conti_$i/stdout.log" ]]
+            do
+                conti="conti_$i"
+                i=$(($i+1))
+            done
+            force_tag=$(grep 'Total force' $conti/stdout.log | tail -n 1 | sed 's/\t/ /')
+            read -ra force_arr <<< $force_tag
+            force=${force_arr[3]}
+            energy_tag=$(grep 'total energy' $conti/stdout.log | grep Ry | tail -n 1 | sed 's/\t/ /')
+            read -ra energy_arr <<< $energy_tag
+            energy=${energy_arr[3]}
+            stat='Continued'
+        else
+            force=''
+            energy=''
+            stat='no data'
         fi
         printf '%-5s %-13s %-14s %-8s\n' $dir $force $energy $stat
         cd ..
