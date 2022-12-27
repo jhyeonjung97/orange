a08ffe94d7bcea3667154e5f658bcfa73fcdb70c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-12-27 09:42:24 -0800

    .

--- conti.sh
+++ conti.sh
@@ -114,8 +114,10 @@ function qe {
     fi
     if [[ -n $(grep walltime run_slurm.sh | grep 120) ]]; then
         sed -i 's/170000/430000/' incar.in
+        sed -i 's/170000/430000/' qe-relax.in
     elif [[ -n $(grep walltime run_slurm.sh | grep 48) ]]; then
         sed -i 's/430000/170000/' incar.in
+        sed -i 's/430000/170000/' qe-relax.in
     fi
     cat incar.in potcar.in poscar.in kpoints.in > qe-relax.in
     if [[ ${here} == 'burning' ]]; then
