829a84ff8712cd019171788b16a151dbfbdd9172
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-12-29 11:06:40 -0800

    .

--- cep-sol.sh
+++ cep-sol.sh
@@ -5,6 +5,9 @@ cp * wave
 sh ~/bin/orange/modify.sh INCAR ISTART 1
 sh ~/bin/orange/modify.sh INCAR LSOL .TRUE.
 sh ~/bin/orange/modify.sh INCAR LWAVE .FALSE.
+sh ~/bin/orange/modify.sh INCAR NSW
+sh ~/bin/orange/modify.sh INCAR IBRION
+
 
 goal=$1
 # goal=-0.6
@@ -82,8 +85,8 @@ range1=$(echo "$goal $error" | awk '{print $1 + $2}')
 until [[ `echo "$range0 < $ep" | bc` -eq 1 ]] && [[ `echo "$ep < $range1" | bc` -eq 1 ]] 
 do
     if [[ ${#map[@]} -ne 0 ]]; then
-        mkdir $ne
-        cp INCAR POSCAR CONTCAR XDATCAR OUTCAR OSZICAR vasprun.xml stdout.log $ne
+        mkdir cep_$ne
+        cp INCAR POSCAR CONTCAR XDATCAR OUTCAR OSZICAR vasprun.xml stdout.log cep_$ne
         # if [[ -s CONTCAR ]]; then
         #     mv CONTCAR POSCAR
         # fi
@@ -133,6 +136,78 @@ do
         sh mpiexe.sh
     else
         exit 1
+    fi
+    update
+    map+=([$ne]=$ep)
+    x1=$x2
+    y1=$y2
+    x2=$ne
+    y2=$ep
+    # linear
+done
+
+sh ~/bin/orange/modify.sh INCAR NSW 600
+sh ~/bin/orange/modify.sh INCAR IBRION 2
+if [[ -s POSCAR ]] && [[ -s WAVECAR ]]; then
+    sh mpiexe.sh
+else
+    exit 2
+fi
+
+until [[ `echo "$range0 < $ep" | bc` -eq 1 ]] && [[ `echo "$ep < $range1" | bc` -eq 1 ]] 
+do
+    if [[ ${#map[@]} -ne 0 ]]; then
+        mkdir opt_$ne
+        cp INCAR POSCAR CONTCAR XDATCAR OUTCAR OSZICAR vasprun.xml stdout.log opt_$ne
+        # if [[ -s CONTCAR ]]; then
+        #     mv CONTCAR POSCAR
+        # fi
+    fi
+    
+    if [[ ${#map[@]} -eq 0 ]]; then
+        type=type0
+        if [[ -n $(grep '#NEDIFF' INCAR) ]]; then
+            diff=0.0
+        elif [[ -n $(grep NEDIFF INCAR) ]]; then
+            read -ra nediff <<< $(grep NEDIFF INCAR)
+            diff=${nediff[2]}
+        else
+            diff=0.0
+        fi
+    elif [[ ${#map[@]} -eq 1 ]] && [[ `echo "$ep < $goal" | bc` == 1 ]]; then
+        type=type1
+        diff=-$step
+    elif [[ ${#map[@]} -eq 1 ]] && [[ `echo "$ep > $goal" | bc` == 1 ]]; then
+        type=type2
+        diff=+$step
+    else
+        grad=$(echo "$x1 $x2 $y1 $y2" | awk '{print ($1 - $2) / ($3 - $4)}')
+        diff=$(echo "$grad $goal $y2" | awk '{print $1 * ($2 - $3)}')
+        if [[ `echo "$diff > 10.0" | bc` == 1 ]]; then
+            type=type3
+            diff=+10.0
+        elif [[ `echo "$diff < -10.0" | bc` == 1 ]]; then
+            type=type4
+            diff=-10.0
+        else
+            type=type5
+        fi
+    fi
+    # echo -e "$x1\t$x2\t$y1\t$y2\t$grad\t$goal\t$type\t$diff" >> check.log
+    new=$(echo "$ne $diff" | awk '{print $1 + $2}')
+    while in_map $new
+    do
+        if [[ `echo "$diff < 0" | bc` == 1 ]]; then
+            new=$(echo "$new $step" | awk '{print $1 - $2}')
+        else
+            new=$(echo "$new $step" | awk '{print $1 + $2}')
+        fi
+    done
+    sh ~/bin/orange/modify.sh INCAR NELECT $new
+    if [[ -s POSCAR ]] && [[ -s WAVECAR ]]; then
+        sh mpiexe.sh
+    else
+        exit 3
     fi
     update
     map+=([$ne]=$ep)
