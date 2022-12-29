8e01ce4c2e65229eb59553c44fc549cd4915c21a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-12-29 09:29:16 -0800

    .

--- run-nurion.sh
+++ run-nurion.sh
@@ -98,6 +98,7 @@ else
         if [[ -s WAVECAR ]]; then
             grep mpiexe run_slurm.sh >> mpiexe.sh
             sed -i -e '/mpiexe/d' run_slurm.sh
+            rm STD*
         elif [[ -s CONTCAR ]]; then
             mkdir geo
             cp * geo
