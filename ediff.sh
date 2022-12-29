8aa96df07d19532dedf4b3e29227af6fd8e24865
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-12-29 11:50:13 -0800

    .

--- ediff.sh
+++ ediff.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 j=1
 while [[ -n $(grep EDIFF stdout.log) ]]; then
-do
+do 
+    i=1
+    save="conti_$i"
+    while [[ -d "conti_$i" ]]
+    do
+        i=$(($i+1))
+        save="conti_$i"
+    done
+    mkdir $save
+    mv * $save
+    cd $save/
+    mv */ ..
+    cp POSCAR ../.POSCAR_ediff
+    cp POSCAR CONTCAR INCAR KPOINTS POTCAR run_slurm.sh .POSCAR_ediff ..
+    cd ..
     ediff=$(grep 'EDIFF ' INCAR | tail -c 2)
     if [[ $j -eq 2 ]]; then
         sh ~/bin/orange/modify.sh INCAR EDIFF 1E-0$(($ediff+1))
@@ -13,3 +27,4 @@ done
 if [[ -n $(grep EDIFF stdout.log) ]]; then
     exit 4
 fi
+
