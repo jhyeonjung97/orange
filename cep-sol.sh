41a4380d26232737e26b1284833f9158c34a930d
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-12-30 22:48:03 -0800

    .

--- cep-sol.sh
+++ cep-sol.sh
@@ -172,7 +172,7 @@ echo -e "Nelect\tType\tShift\tFermi\tWork.F\tPotential" >> optout.log
 
 while read line
 do
-    read -a line <<< $line
+    IFS=$'\t' read -r -a line <<< $line
     head=${line[0]}
     head=${head#-}
     head=${head//[0-9]/}
@@ -182,13 +182,16 @@ do
     if [[ -z $head ]] && [[ ${#line[@]} == 7 ]]; then
         ne=${line[0]}
         ep=${line[6]}
-        map+=([$ne]=$ep)
         x1=$x2
         y1=$y2
         x2=$ne
         y2=$ep
-        echo ${line[@]}
-        echo $x1 $x2 $y1 $y2
+        if [[ "$x1" == "$x2" ]]; then
+            x1=''
+            y1=''
+        else
+            map+=([$ne]=$ep)
+        fi
     fi
 done < optout.log
 
@@ -225,12 +228,12 @@ do
     else
         grad=$(echo "$x1 $x2 $y1 $y2" | awk '{print ($1 - $2) / ($3 - $4)}')
         diff=$(echo "$grad $goal $y2" | awk '{print $1 * ($2 - $3)}')
-        if [[ `echo "$diff > 10.0" | bc` == 1 ]]; then
+        if [[ `echo "$diff > 1.0" | bc` == 1 ]]; then
             type=type3
-            diff=+10.0
-        elif [[ `echo "$diff < -10.0" | bc` == 1 ]]; then
+            diff=+1.0
+        elif [[ `echo "$diff < -1.0" | bc` == 1 ]]; then
             type=type4
-            diff=-10.0
+            diff=-1.0
         else
             type=type5
         fi
@@ -260,3 +263,4 @@ do
     y2=$ep
     # linear
 done
+IFS=$OIFS
