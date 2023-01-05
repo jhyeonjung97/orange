9ada06bb0f729d54fb2164f6f90c3dc5a18f685d
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-01-05 00:19:54 -0800

    .

--- spread.sh
+++ spread.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-
 if [[ -z $2 ]]; then
     file=$1
     SET='*/'
-else
-    file=$2
+elif [[ -z ${$2//[0-9]/} ]]; then
+    file=${@:2}
     for i in $(seq 2 $1); do
         SET+='*/'
     done
 fi
 
 for dir in $SET; do
-    if [[ ! -e $file ]]; then
-        name=${file%.*}
-        ext=${file##*.}
-        numb=$(echo $dir | cut -c 1)
-        # echo $name $ext $numb
-        cp $name$numb.$ext $dir$name.$ext
-    else
-        cp $file $dir
-    fi
+    cp $file $dir
+    # if [[ ! -e $file ]]; then
+    #     name=${file%.*}
+    #     ext=${file##*.}
+    #     numb=$(echo $dir | cut -c 1)
+    #     # echo $name $ext $numb
+    #     cp $name$numb.$ext $dir$name.$ext
+    # else
+    #     cp $file $dir
+    # fi
     # if [[ $file == 'run_slurm.sh' ]]
     #     cp .run_conti.sh $dir
     # fi
