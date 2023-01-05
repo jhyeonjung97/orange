3194233c63aafe006eb23906c377616a9b6738a1
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-01-05 15:23:57 -0800

    .

--- alias-basic.sh
+++ alias-basic.sh
@@ -10,6 +10,7 @@ alias pos2cif='~/bin/vtstscripts/pos2cif.pl POSCAR'
 alias con2cif='~/bin/vtstscripts/pos2cif.pl CONTCAR'
 alias out2xyz='sh ~/bin/orange/out2xyz.sh'
 alias rmport='~/bin/rm_mv ~/bin/port/*'
+alias lsport='ls ~/bin/port/'
 alias rmtemp='~/bin/rm_mv ~/bin/temp.sh'
 alias vitemp='vi ~/bin/temp.sh'
 
