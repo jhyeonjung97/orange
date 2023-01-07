c80d946b1b3083148d30d73d5f37f957d9a2bccd
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-01-07 15:26:37 -0800

    .

--- mmff.sh
+++ mmff.sh
@@ -36,7 +36,7 @@ do
         if [[ $ext != 'mol2' ]]; then
             obabel $name$i.$ext -O $name$i.mol2
         fi
-        echo $file
+        echo $file $name$i.mol2 $name$i.pdb
         obminimize -n 10000000000 -sd -c 1e-10 -ff MMFF94s $name$i.mol2 > $name$i.pdb
         # python ~/bin/orange/cluster.py $name$i.pdb $name$i.xyz $a
     fi
