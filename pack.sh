3884a1df0f3a9e67200d8f446f107db2dc4a3c23
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-01-06 23:29:48 -0800

    .

--- pack.sh
+++ pack.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-~/bin/packmol/packmol < $1
+if [[ $1 =~ '-h' ]]; then
+    echo 'usage: pack.sh [filename.inp] [seed]'
+    exit 1
+fi
+
+if [[ -z $3 ]]; then
+    SET=$(seq 1 $2)
+else
+    SET=$(seq $2 $3)
+fi
 
 filename="${1%.*}"
-extension="${1##*.}"
+for i in $SET
+do
+    sed -i -e "/output/c\output $filename$i.xyz" $1
+    sed -i -e "/seed/c\seed $i" $1
+    ~/bin/packmol/packmol < $1
+done
 
-mv mixture.xyz $filename.xyz
-
-python3 ~/bin/orange/xyz2hex.py $filename.xyz
-
-#echo "scp $filename.vasp hailey@134.79.69.172:~/Desktop/au"
-#scp $filename.vasp hailey@134.79.69.172:~/Desktop/au
-
-sed -n 6p $filename.vasp >> temp1
-sed 1d $filename.vasp >> temp2
-cat temp1 temp2 > $filename.vasp
-rm temp1 temp2
-
-echo "cp $filename.vasp ~/port/"
-cp $filename.vasp ~/port/
+read -p "lattice parameter (A): " a
+if [[ -z $a ]]; then
+    echo 'use default lattice parameter, 40 A ...'
+    a=40.
+elif [[ ! $a =~ '.' ]]; then
+    a=$a.
+fi
+python3 ~/bin/orange/convert.py xyz vasp $a
