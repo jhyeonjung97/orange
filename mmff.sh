c52a8e3493e1d6fa59ca762c0d831cf2edc573c1
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-01-08 13:24:55 -0800

    .

--- mmff.sh
+++ mmff.sh
@@ -38,6 +38,8 @@ do
             echo "obabel $name$i.$ext -O $name$i.mol2"
         fi
         obminimize -n 10000000000 -sd -c 1e-10 -ff MMFF94s $name$i.mol2 > $name$i.pdb
+                obminimize -n 100 -sd -c 1e-10 -ff MMFF94s h510.mol2 > h510.pdb
+
         echo "obminimize -n 10000000000 -sd -c 1e-10 -ff MMFF94s $name$i.mol2 > $name$i.pdb"
         # python ~/bin/orange/cluster.py $name$i.pdb $name$i.xyz $a
     fi
