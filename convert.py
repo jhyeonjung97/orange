d090470348bd08822003e7c01bc73d1e42c3c433
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-01-08 14:55:39 -0800

    .

--- convert.py
+++ convert.py
@@ -7,11 +7,23 @@ if not argv[2]:
     print('wrong usage: convert (type1) (type2) [lattice]')
     exit()
 
-if len(argv) == 4:
+if len(argv) == 6:
     a = float(argv[3])
+    b = float(argv[4])
+    c = float(argv[5])
+elif len(argv) == 5:
+    a = float(argv[3])
+    b = a
+    c = float(argv[4])
+elif len(argv) == 4:
+    a = float(argv[3])
+    b = a
+    c = a
 else:
-    print('use default lattice parameter, 30 A ...')
+    print('use default lattice parameter (30 A) for cubic cell...')
     a = 30.
+    b = a
+    c = a
 
 # iterating over all files
 for file in os.listdir('./'):
@@ -19,7 +31,7 @@ for file in os.listdir('./'):
         atoms = read(file)
         # del atoms[[atom.symbol == 'Li' for atom in atoms]]
         atoms = sort(atoms)
-        atoms.set_cell([a, a, a])
+        atoms.set_cell([a, b, c])
         # atoms.set_cell([30., 30., 30., 90., 90., 90.])
         atoms.center()
         write(file.replace('%s' %argv[1], '%s' %argv[2]), atoms, format='%s' %argv[2])
