862ab1271c3e074594862356cd715a3bd4a03c5c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-02-20 15:53:39 +0900

    .

--- ediff.sh
+++ ediff.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 i=1
 j=1
-while [[ -n $(grep EDIFF stdout.log) ]] && [[ $j -le 3 ]]
+while [[ -n $(tail stdout.log | grep EDIFF) ]] && [[ $j -le 3 ]]
 do 
     i=1
     save="conti_$i"
@@ -18,7 +18,6 @@ do
     if [[ $j -eq 2 ]]; then
         sh ~/bin/orange/modify.sh INCAR EDIFF 1E-0$(($ediff+1))
     fi
-    rm stdout.log
     sh mpiexe.sh
     j=$(($j+1))
 done
