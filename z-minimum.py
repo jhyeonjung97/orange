c64588fe9ab88700b82e1fcbbf8c74eeece31efe
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-10 01:46:33 +0900

    .

--- z-minimum.py
+++ z-minimum.py
@@ -16,6 +16,6 @@ for atoms in traj:
     i = i+1
     x.append(i)
     y.append(min(list))
-    print(list)
+    print(min(list))
 
 # np.savetxt("%s.csv" % filename, np.transpose([x, y]), delimiter =", ", fmt ='% s')
