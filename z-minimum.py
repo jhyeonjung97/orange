2c9ef6e987c174f8b311c9a538db1db2f4e45cc5
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-10 01:49:54 +0900

    .

--- z-minimum.py
+++ z-minimum.py
@@ -1,21 +1,26 @@
 from ase.io.vasp import read_vasp_xdatcar
+from ase.io import read, write
 from statistics import mean
 import numpy as np
 import sys
 
 #element = input("which element? ")
 # filename = input("filename? ")
-traj = read_vasp_xdatcar('XDATCAR', index = 0)
+# traj = read_vasp_xdatcar('XDATCAR', index = 0)
 
 i = 0
 x = []
 y = []
 
-for atoms in traj:
-    list = [atom.z for atom in atoms if atom.symbol == 'H' and atom.z < 8]
-    i = i+1
-    x.append(i)
-    y.append(min(list))
-    print(min(list))
+# for atoms in traj:
+#     list = [atom.z for atom in atoms if atom.symbol == 'H' and atom.z < 8]
+#     i = i+1
+#     x.append(i)
+#     y.append(min(list))
+#     print(min(list))
 
+atoms = read(CONTCAR)
+list = [atom.z for atom in atoms if atom.symbol == 'H']
+print(min(list))
+    
 # np.savetxt("%s.csv" % filename, np.transpose([x, y]), delimiter =", ", fmt ='% s')
