ec23a3a61278b964609fb85ab363baf000078c9c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-10 06:05:21 +0900

    .

--- chg.py
+++ chg.py
@@ -4,31 +4,22 @@ from sys import argv
 
 data = [] 
 length = []
-number = int(argv[1])
-filename = argv[2]
+filename = argv[1]
 
 # for i in range(1,number+1):
 #     chg = np.loadtxt("ACF%s.dat" % i, dtype=str)[:,4]
 #     data.append(chg)
 
-for file in os.listdir('./'):
-    if file.startswith('ACF') and file.endswith('.dat'):
-        chg = np.loadtxt(file, dtype=str)[:,4]
-        chg.shape
-        length.append(len(chg))
+# for file in os.listdir('./'):
+#     if file.startswith('ACF') and file.endswith('.dat'):
+#         chg = np.loadtxt(file, dtype=str)[:,4]
+#         chg.shape
+#         length.append(len(chg))
         
 for file in os.listdir('./'):
     if file.startswith('ACF') and file.endswith('.dat'):
         chg = np.loadtxt(file, dtype=str)[:,4]
-        data.append(chg.reshape(1,max(length)))
-        
-# data = 
-#         data.append(chg)
+        data.append(chg)
 
-# # data.shape
-# # data.ndim
-# data_reshaped = data.reshape(-1,-1)
-# data_reshaped.shape
-# data_reshaped.ndim
 # reshape.data(data.ndim,max(length))
-# np.savetxt("%s.csv" % filename, np.transpose(data), delimiter =", ", fmt ='% s')
+np.savetxt("%s.csv" % filename, np.transpose(data), delimiter =", ", fmt ='% s')
--- chg.sh
+++ chg.sh
@@ -1,11 +1,9 @@
-i=0
 filename=$1
 
 for dir in */
 do
     numb=$(echo $dir | cut -c 1)
     cp $dir/ACF.dat ACF$numb.dat
-    i+=1
 done
 
 sed -i -e '1,2d' ACF*.dat
@@ -15,4 +13,4 @@ sed -i -e '$d' ACF*.dat
 sed -i -e '$d' ACF*.dat
 sed -i -e '$d' ACF*.dat
 
-python ~/bin/orange/chg.py $i $filename
+python ~/bin/orange/chg.py $filename
