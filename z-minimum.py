86e949a51ed6f9420cb605afff634e01c16cfee3
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-10 02:09:53 +0900

    .

--- z-minimum.py
+++ z-minimum.py
@@ -20,7 +20,7 @@ y = []
 #     print(mean(list))
 
 atoms = read('CONTCAR')
-list = [atom.z for atom in atoms if atom.symbol == 'H']
+list = [atom.z for atom in atoms if atom.symbol == 'H' and 7 < atom.z < 7.5]
 print(min(list))
     
 # np.savetxt("%s.csv" % filename, np.transpose([x, y]), delimiter =", ", fmt ='% s')
