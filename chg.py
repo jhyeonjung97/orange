e33c7074dae59c7c139f91d11720b1f6e03f5700
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-10 05:24:59 +0900

    .

--- chg.py
+++ chg.py
@@ -5,8 +5,12 @@ data = []
 number = int(argv[1])
 filename = argv[2]
 
-for i in range(1,number+1):
-    chg = np.loadtxt("ACF%s.dat" % i, dtype=str)[:,4]
+# for i in range(1,number+1):
+#     chg = np.loadtxt("ACF%s.dat" % i, dtype=str)[:,4]
+#     data.append(chg)
+
+for file in os.listdir('./'):
+    chg = np.loadtxt(file, dtype=str)[:,4]
     data.append(chg)
 
 np.savetxt("%s.csv" % filename, np.transpose(data), delimiter =", ", fmt ='% s')
