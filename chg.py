12313e6112eba743fcaccbef3c9aeca3cce27277
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-11 23:10:02 +0900

    .

--- chg.py
+++ chg.py
@@ -4,7 +4,8 @@ from sys import argv
 
 data = [] 
 length = []
-filename = argv[1]
+number = argv[1]
+filename = argv[2]
 
 # for i in range(1,number+1):
 #     chg = np.loadtxt("ACF%s.dat" % i, dtype=str)[:,4]
@@ -16,10 +17,16 @@ filename = argv[1]
 #         chg.shape
 #         length.append(len(chg))
         
-for file in os.listdir('./'):
-    if file.startswith('ACF') and file.endswith('.dat'):
-        chg = np.loadtxt(file, dtype=str)[:,4]
-        data.append(chg)
+for i in range(1,number+1):
+    print("ACF%d.csv" % i)
+    chg = np.loadtxt("ACF%d.csv" % i, dtype=str)[:,4]
+    print(chg)
+    data.append(chg)
+
+# for file in os.listdir('./'):
+#     if file.startswith('ACF') and file.endswith('.dat'):
+#         chg = np.loadtxt(file, dtype=str)[:,4]
+#         data.append(chg)
 
 # reshape.data(data.ndim,max(length))
 np.savetxt("%s.csv" % filename, np.transpose(data), delimiter =", ", fmt ='% s')
--- chg.sh
+++ chg.sh
@@ -1,9 +1,11 @@
+i=0
 filename=$1
 
 for dir in */
 do
     numb=$(echo $dir | cut -c 1)
     cp $dir/ACF.dat ACF$numb.dat
+    i=i+1
 done
 
 sed -i -e '1,2d' ACF*.dat
@@ -12,4 +14,4 @@ sed -i -e '$d' ACF*.dat
 sed -i -e '$d' ACF*.dat
 sed -i -e '$d' ACF*.dat
 
-python ~/bin/orange/chg.py $filename
+python ~/bin/orange/chg.py $i $filename
