8f7b5195b15d4fc8e5e0148dcb78d473d3098c2a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-17 02:23:39 +0900

    .

--- z-minimum.py
+++ z-minimum.py
@@ -13,14 +13,9 @@ x = []
 y = []
 
 for atoms in traj:
-    list = [atoms[63].z,
-            atoms[64].z, 
-            atoms[65].z, 
-            atoms[67].z, 
-            atoms[69].z, 
-            atoms[70].z,
-            atoms[71].z, 
-            atoms[76].z]
+    # list = [atoms[63].z,atoms[64].z,atoms[65].z,atoms[67].z,atoms[69].z,atoms[70].z,atoms[71].z,atoms[76].z]
+    list = [atoms[67].z,atoms[72].z,atoms[75].z,atoms[76].z,atoms[78].z,atoms[80].z,atoms[83].z,atoms[84].z]
+    
     # list = [atom.z for atom in atoms if atom.symbol == 'H' and atom.z < 8]
     i = i+1
     x.append(i)
