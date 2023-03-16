0f3c716229ff47594ed5bb50ebc7210464e1d1d6
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-17 02:11:11 +0900

    .

--- z-minimum.py
+++ z-minimum.py
@@ -6,22 +6,30 @@ import sys
 
 # element = input("which element? ")
 # filename = input("filename? ")
-# traj = read_vasp_xdatcar('XDATCAR', index = 0)
+traj = read_vasp_xdatcar('XDATCAR', index = 0)
 
 i = 0
 x = []
 y = []
 
-# for atoms in traj:
-#     list = [atom.z for atom in atoms if atom.symbol == 'H' and atom.z < 8]
-#     # i = i+1
-#     # x.append(i)
-#     # y.append(min(list))
-#     print(mean(list))
+for atoms in traj:
+    list = [atoms[63].z,
+            atoms[64].z, 
+            atoms[65].z, 
+            atoms[67].z, 
+            atoms[69].z, 
+            atoms[70].z,
+            atoms[71].z, 
+            atoms[76].z]
+    # list = [atom.z for atom in atoms if atom.symbol == 'H' and atom.z < 8]
+    # i = i+1
+    # x.append(i)
+    # y.append(min(list))
+    print(mean(list))
 
-atoms = read('CONTCAR')
-#list = [atom.z for atom in atoms if atom.symbol == 'H' and atom.z > 7]
-list = [atoms[0].z, atoms[1].z]
-print(min(list))
+# atoms = read('CONTCAR')
+# #list = [atom.z for atom in atoms if atom.symbol == 'H' and atom.z > 7]
+# list = [atoms[0].z, atoms[1].z]
+# print(min(list))
     
 # np.savetxt("%s.csv" % filename, np.transpose([x, y]), delimiter =", ", fmt ='% s')
