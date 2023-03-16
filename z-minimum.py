1568462e3e85848e5de160a7872230f100b3fd1b
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-17 02:47:35 +0900

    .

--- z-minimum.py
+++ z-minimum.py
@@ -18,8 +18,9 @@ for atoms in traj:
     # list = [atoms[78].z,atoms[83].z,atoms[87].z,atoms[89].z,atoms[90].z,atoms[92].z,atoms[94].z,atoms[96].z]
     # list = [atoms[64].z,atoms[71].z,atoms[65].z,atoms[69].z,atoms[66].z,atoms[72].z,atoms[81].z,
     #         atoms[78].z,atoms[77].z,atoms[75].z,atoms[82].z,atoms[73].z,atoms[80].z,atoms[84].z]
-    list = [atoms[64].z,atoms[63].z,atoms[89].z,atoms[65].z,atoms[72].z,atoms[71].z,
-            atoms[68].z,atoms[69].z,atoms[93].z,atoms[70].z,atoms[73].z,atoms[66].z]
+    list = [atoms[90].z,atoms[92].z,atoms[106].z,atoms[82].z,atoms[99].z,atoms[77].z,atoms[111].z,atoms[101].z,
+            atoms[96].z,atoms[88].z,atoms[108].z,atoms[84].z,atoms[100].z,atoms[83].z,atoms[109].z,atoms[105].z,
+            z,atoms[75].z,atoms[78].z,atoms[97].z,atoms[104].z,atoms[89].z,atoms[94].z]
 
 
     
