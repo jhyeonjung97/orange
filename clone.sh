f35245ea80fc9f8655a56f9db88e7220b0ad8cee
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-20 19:17:08 +0900

    .

--- clone.sh
+++ clone.sh
@@ -1,13 +1,24 @@
 # usage: sh clone.sh a2.vasp b3 -> b1.vasp b2.vasp b3.vasp
+#      : sh clone.sh a.vasp c.vasp
 
-# filename=$(echo $2 | rev | cut -c 2- | rev)
-# numb=$(echo $2 | rev | cut -c 1)
-
-filename=$(echo ${2: (-1)})
-numb=$(echo ${2: 0:(-1)})
-ext=${1##*.}
-
-for i in $(seq 1 $numb)
-do
-    cp $1 $filename$i.$ext
-done
+if [[ -n $(echo $2 | grep .) ]]; then
+    filename=${1%.*}
+    ext=${1##*.}
+    mkdir clone_temp
+    cp "$filename"*.$ext clone_temp
+    cd clone_temp
+    sh ~/bin/orange/rename.sh $1 $2
+    mv * ..
+    cd ..
+    rm -r clone_temp
+else
+    filename=$(echo $2 | rev | cut -c 2- | rev)
+    numb=$(echo $2 | rev | cut -c 1)
+    # filename=$(echo ${2: (-1)})
+    # numb=$(echo ${2: 0: (-1)})
+    ext=${1##*.}
+    for i in $(seq 1 $numb)
+    do
+        cp $1 $filename$i.$ext
+    done
+fi
