bba4976a89a3c6e2075de1bc323a1f9a7f5d6c87
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-22 06:56:56 +0900

    .

--- rename.sh
+++ rename.sh
@@ -5,15 +5,32 @@ if [[ -z $1 ]] || [[ -z $2 ]]; then
     exit 1
 fi
 
-extension1="${1##*.}"
-filename1="${1%.*}"
-
-extension2="${2##*.}"
-filename2="${2%.*}"
-
-for i in {0..9}
-do
-    if [[ -e $filename1$i.$extension1 ]]; then
-        mv $filename1$i.$extension1 $filename2$i.$extension2
-    fi
-done
+if [[ $1 == '-ase' ]] || [[ $1 == '-a' ]]; then
+    extension="${2##*.}"
+    filename="${2%.*}"
+    for file in $filename*.$extenstion
+    do
+        # name=$(echo $file | rev | cut -c 6- | rev)
+        numb=$(echo $file | rev | cut -c -5 | rev)
+        if [[ $numb == 00000 ]]; then
+            cp $file "$filename"1.$extension
+        elif [[ $numb == 0000* ]]; then
+            i = $(echo $file | rev | cut -c -1 | rev)
+            cp $file $filename$i.$extension
+        elif [[ $numb == 000* ]]; then
+            i = $(echo $file | rev | cut -c -2 | rev)
+            cp $file $filename$i.$extension
+        fi
+    done
+else
+    extension1="${1##*.}"
+    filename1="${1%.*}"
+    extension2="${2##*.}"
+    filename2="${2%.*}"
+    for i in {0..9}
+    do
+        if [[ -e $filename1$i.$extension1 ]]; then
+            mv $filename1$i.$extension1 $filename2$i.$extension2
+        fi
+    done
+fi
