8396e305a69c2280b2dfabd749817c65ac22f6b3
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-22 18:55:17 +0900

    .

--- merge-csvs.py
+++ merge-csvs.py
@@ -1,5 +1,39 @@
+# import os
+# import numpy as np
+# import pandas as pd
+
+# # Set the parent directory path where the CSV files are located to the current working directory
+# parent_dir = './'
+
+# # Get a list of all the subdirectories in the parent directory
+# subdirs = [d for d in os.listdir(parent_dir) if os.path.isdir(os.path.join(parent_dir, d))]
+
+# # Create an empty list to store the concatenated data
+# data = []
+
+# # Loop through each subdirectory and concatenate the CSV files
+# for subdir in subdirs:
+#     subdir_path = os.path.join(parent_dir, subdir)
+#     for f in os.listdir(subdir_path):
+#         print(f)
+#         if os.path.isfile(os.path.join(subdir_path, f)) and f.endswith('.csv'):
+#             print(f)
+#             subdir_data = np.loadtxt(os.path.join(subdir_path, f), dtype=str, delimiter=',')
+#             print(os.path.join(subdir_path, f))
+#             print(subdir_data)
+#             data.append(subdir_data)
+            
+# # for i in range(1,1+1):
+# #     path = "./{i}/"
+# #     for f in os.listdir(path):
+# #         if os.path.isfile(os.path.join(path, f)) and f.endswith('.csv'):
+# #             chg = np.loadtxt(f, dtype=str)
+# #             data.append(subdir_data)
+
+# np.savetxt("merged.csv", np.transpose(data), delimiter =", ", fmt ='% s')
+# print(data)
+
 import os
-import numpy as np
 import pandas as pd
 
 # Set the parent directory path where the CSV files are located to the current working directory
@@ -14,21 +48,9 @@ data = []
 # Loop through each subdirectory and concatenate the CSV files
 for subdir in subdirs:
     subdir_path = os.path.join(parent_dir, subdir)
-    for f in os.listdir(subdir_path):
-        print(f)
-        if os.path.isfile(os.path.join(subdir_path, f)) and f.endswith('.csv'):
-            print(f)
-            subdir_data = np.loadtxt(os.path.join(subdir_path, f), dtype=str, delimiter=',')
-            print(os.path.join(subdir_path, f))
-            print(subdir_data)
-            data.append(subdir_data)
-            
-# for i in range(1,1+1):
-#     path = "./{i}/"
-#     for f in os.listdir(path):
-#         if os.path.isfile(os.path.join(path, f)) and f.endswith('.csv'):
-#             chg = np.loadtxt(f, dtype=str)
-#             data.append(subdir_data)
-
-np.savetxt("merged.csv", np.transpose(data), delimiter =", ", fmt ='% s')
-print(data)
+    subdir_data = pd.concat([pd.read_csv(os.path.join(subdir_path, f)) for f in os.listdir(subdir_path) if f.endswith('.csv')])
+    data.append(subdir_data)
+    
+# Concatenate all the dataframes in the list and save to a new CSV file
+concatenated_data = pd.concat(data)
+concatenated_data.to_csv('merged.csv', index=False)
