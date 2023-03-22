1f3da6d8ad257f0a3605390e2ca8a8166c2447a8
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-22 15:45:21 +0900

    .

--- merge-csvs.py
+++ merge-csvs.py
@@ -1,31 +1,47 @@
 import os
 import pandas as pd
 
-# Get the input file name (excluding file extension) from the user
-input_name = input("Enter the name of the input CSV file (excluding file extension): ")
-
-# Set the parent directory path where the CSV files are located
+# Set the parent directory path where the CSV files are located to the current working directory
 parent_dir = './'
 
 # Get a list of all the subdirectories in the parent directory
 subdirs = [d for d in os.listdir(parent_dir) if os.path.isdir(os.path.join(parent_dir, d))]
 
+# Loop through each subdirectory and print its CSV files
+for subdir in subdirs:
+    print(f"CSV files in {subdir}:")
+    files = os.listdir(os.path.join(parent_dir, subdir))
+    for file in files:
+        if file.endswith('.csv'):
+            print(f"  {file}")
+    print()
+
+# Get the input file name (excluding file extension) from the user
+input_name = input("Enter the name of the input CSV file (excluding file extension): ")
+
+# Get a list of all the CSV files in the subdirectories that start with the input name
+csv_files = []
+for subdir in subdirs:
+    subdir_path = os.path.join(parent_dir, subdir)
+    subdir_csv_files = [f for f in os.listdir(subdir_path) if os.path.isfile(os.path.join(subdir_path, f)) and f.endswith('.csv') and f.startswith(input_name)]
+    csv_files.extend([os.path.join(subdir_path, f) for f in subdir_csv_files])
+
 # Create an empty list to store the dataframes
 dfs = []
 
-# Loop through each subdirectory and read in the CSV file as a dataframe, then append it to the list of dataframes
-for subdir in subdirs:
-    file_path = os.path.join(parent_dir, subdir, input_name + ".csv")
-    df = pd.read_csv(file_path)
+# Loop through each CSV file and read it into a dataframe, then append it to the list of dataframes
+for csv_file in csv_files:
+    df = pd.read_csv(csv_file)
     dfs.append(df)
 
 # Concatenate all the dataframes into one
 result = pd.concat(dfs)
 
-# Get the output file name (including file extension) from the user
+# Set the output file name as the input name with ".csv" added to the end
 output_name = input_name + ".csv"
 
 # Save the concatenated dataframe to a new CSV file
 result.to_csv(output_name, index=False)
 
 print("CSV files have been concatenated and saved to", output_name)
+
