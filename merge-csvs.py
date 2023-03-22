7d1a327e3829cc9d2e9069af37f2c4d6b8f40112
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-22 17:06:16 +0900

    .

--- merge-csvs.py
+++ merge-csvs.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 
 # Set the parent directory path where the CSV files are located to the current working directory
@@ -7,21 +8,13 @@ parent_dir = './'
 # Get a list of all the subdirectories in the parent directory
 subdirs = [d for d in os.listdir(parent_dir) if os.path.isdir(os.path.join(parent_dir, d))]
 
-# Create an empty dataframe to store the concatenated data
-result = pd.DataFrame()
+# Create an empty list to store the concatenated data
+data = []
 
 # Loop through each subdirectory and append the CSV files to the concatenated data
 for subdir in subdirs:
     subdir_path = os.path.join(parent_dir, subdir)
     subdir_csv_files = [f for f in os.listdir(subdir_path) if os.path.isfile(os.path.join(subdir_path, f)) and f.endswith('.csv')]
-    for csv_file in subdir_csv_files:
-        df = pd.read_csv(os.path.join(subdir_path, csv_file))
-        result = result.append(df)
-
-# Set the output file name as "merged.csv"
-output_name = "merged.csv"
-
-# Save the concatenated dataframe to a new CSV file
-result.to_csv(output_name, index=False)
-
-print("CSV files have been merged and saved to", output_name)
+    print(subdir_csv_files)
+    df_csv_concat = pd.concat([pd.read_csv(file) for file in subdir_csv_files ], ignore_index=True)
+    df_csv_concat
