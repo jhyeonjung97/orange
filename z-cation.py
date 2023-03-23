a6928d235279deda652169bbc04cf6271836fec0
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-23 15:31:35 +0900

    .

--- z-cation.py
+++ z-cation.py
@@ -11,62 +11,20 @@ cation_cutoffs = {'Li': 2.5,
 
 # Read the XDATCAR file
 structures = read_vasp_xdatcar('XDATCAR', index=0)
-cell = structures[0].cell
 
 # Find the cation symbol
 cation = None
 for atom in structures[0]:
     if atom.symbol in cation_cutoffs:
         cation = atom.symbol
-        cutoff = cation_cutoffs[cation]
+        # cutoff = cation_cutoffs[cation]
         break
 else:
     raise ValueError('Could not find cation in XDATCAR')
 
 min_z_positions = []
-for i, atoms in enumerate(structures):
-    # Get the indices of cation and water oxygen atoms
-    cation_indices = [j for j, atom in enumerate(atoms) if atom.symbol == cation]
-    water_oxygen_indices = [j for j, atom in enumerate(atoms) if atom.symbol == 'O']
-
-    min_z_position = None
-    for cation_index in cation_indices:
-        for water_oxygen_index in water_oxygen_indices:
-            # Calculate the distance between cation and water oxygen
-            dr = atoms[water_oxygen_index].position - atoms[cation_index].position
-
-            # Apply minimum image convention to account for periodic boundary conditions
-            for m in range(2):
-                if abs(dr[m]) > cell[m,m]/2:
-                    if dr[m] > 0:
-                        dr[m] -= cell[m,m]
-                    else:
-                        dr[m] += cell[m,m]
-
-            distance = np.linalg.norm(dr)
-
-            if distance <= cutoff:
-                # Get the indices of hydrogen atoms in the water molecule
-                hydrogen_indices = []
-                for j, atom in enumerate(atoms):
-                    if atom.symbol == 'H':
-                        dr = atom.position - atoms[water_oxygen_index].position
-                        for m in range(2):
-                            if abs(dr[m]) > cell[m,m]/2:
-                                if dr[m] > 0:
-                                    dr[m] -= cell[m,m]
-                                else:
-                                    dr[m] += cell[m,m]
-                        if np.linalg.norm(dr) <= 1.2:
-                            hydrogen_indices.append(j)
-
-                # Get the minimum z-position of hydrogen atoms in the water molecule
-                z_positions = [atoms[j].position[2] for j in hydrogen_indices]
-                if z_positions:
-                    min_z = min(z_positions)
-                    if min_z_position is None or min_z < min_z_position:
-                        min_z_position = min_z
-
+for atoms in structures:
+    min_z_position = [atom.position[2] for atom in atoms if atom.symbol == cation]
     min_z_positions.append(min_z_position)
     print(f"Iteration {i}: {min_z_position}")
 
