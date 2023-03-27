0e0232e3ceb48e9416543fb099fd97abaa095a81
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-27 19:52:12 +0900

    .

--- z-hydrogen.py
+++ z-hydrogen.py
@@ -26,45 +26,41 @@ else:
 min_z_positions = []
 for i, atoms in enumerate(structures):
     
-    min_z_position = 40
-    cation_hydrogen_indices = []
-    
-    # Get the indices of cation and water oxygen atoms
-    cation_indices = [j for j, atom in enumerate(atoms) if atom.symbol == cation]
-    water_oxygen_indices = [j for j, atom in enumerate(atoms) if atom.symbol == 'O']
-
-    for cation_index in cation_indices:
-        for water_oxygen_index in water_oxygen_indices:
-            # Calculate the distance between cation and water oxygen
-            dr = atoms[water_oxygen_index].position - atoms[cation_index].position
-
-            # Apply minimum image convention to account for periodic boundary conditions
-            for m in range(2):
-                while abs(dr[m]) > cell[m,m]/2:
-                    if dr[m] > 0:
-                        dr -= cell[m]
-                    else:
-                        dr += cell[m]
-
-            distance = np.linalg.norm(dr)
-            
-            if distance <= cutoff:
-                # Get the indices of hydrogen atoms in the water molecule
-                hydrogen_indices = [j for j, atom in enumerate(atoms) if atom.symbol == 'H']
-                
-                for hydrogen_index in hydrogen_indices:
-                        dr = atoms[hydrogen_index].position - atoms[water_oxygen_index].position
-                        for m in range(2):
-                            while abs(dr[m]) > cell[m,m]/2:
-                                if dr[m] > 0:
-                                    dr -= cell[m]
-                                else:
-                                    dr += cell[m]
-                        if np.linalg.norm(dr) <= 1.2:
-                            cation_hydrogen_indices.append(hydrogen_index)
+    if i == 0:
+        min_z_position = None
+        cation_hydrogen_indices = []
+        # Get the indices of cation and water oxygen atoms
+        cation_indices = [j for j, atom in enumerate(atoms) if atom.symbol == cation]
+        water_oxygen_indices = [j for j, atom in enumerate(atoms) if atom.symbol == 'O']
+        for cation_index in cation_indices:
+            for water_oxygen_index in water_oxygen_indices:
+                # Calculate the distance between cation and water oxygen
+                dr = atoms[water_oxygen_index].position - atoms[cation_index].position
+                # Apply minimum image convention to account for periodic boundary conditions
+                for m in range(2):
+                    while abs(dr[m]) > cell[m,m]/2:
+                        if dr[m] > 0:
+                            dr -= cell[m]
+                        else:
+                            dr += cell[m]
+                distance = np.linalg.norm(dr)
+                if distance <= cutoff:
+                    # Get the indices of hydrogen atoms in the water molecule
+                    hydrogen_indices = [j for j, atom in enumerate(atoms) if atom.symbol == 'H']
+                    for hydrogen_index in hydrogen_indices:
+                            dr = atoms[hydrogen_index].position - atoms[water_oxygen_index].position
+                            for m in range(2):
+                                while abs(dr[m]) > cell[m,m]/2:
+                                    if dr[m] > 0:
+                                        dr -= cell[m]
+                                    else:
+                                        dr += cell[m]
+                            if np.linalg.norm(dr) <= 1.2:
+                                cation_hydrogen_indices.append(hydrogen_index)
 
     # Get the minimum z-position of hydrogen atoms in the water molecule
     min_z_position = min([atoms[j].position[2] for j in cation_hydrogen_indices])
+    if min_z_position == None
     min_z_positions.append(min_z_position)
     print(f"Iteration {i}: {min_z_position}")
     
