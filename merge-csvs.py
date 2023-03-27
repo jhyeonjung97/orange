a4cb073abcd06241fa891281d197f62ecc3bbddf
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-27 14:56:08 +0900

    .

--- merge-csvs.py
+++ merge-csvs.py
@@ -1,17 +1,12 @@
 import os
 import numpy as np
+from sys import argv
 
 # Set the parent directory path where the CSV files are located to the current working directory
 parent_dir = './'
 
 # Get a list of all the subdirectories in the parent directory
 subdirs = sorted([d for d in os.listdir(parent_dir) if os.path.isdir(os.path.join(parent_dir, d))])
-
-# Print the list of CSV files inside each subdirectory
-for subdir in subdirs:
-    subdir_path = os.path.join(parent_dir, subdir)
-    csv_files = [f[:-4] for f in sorted(os.listdir(subdir_path)) if os.path.isfile(os.path.join(subdir_path, f)) and f.endswith('.csv')]
-    print(f"CSV files in {subdir_path}: {', '.join(csv_files)}")
 
 # Get the name of the CSV files to merge from the user
 csv_name = input("Enter the name of the CSV files to merge (without the '.csv' extension): ")
@@ -24,7 +19,7 @@ for subdir in subdirs:
     subdir_path = os.path.join(parent_dir, subdir)
     for f in sorted(os.listdir(subdir_path)):
         if os.path.isfile(os.path.join(subdir_path, f)) and f.startswith(csv_name) and f.endswith('.csv'):
-            subdir_data = np.loadtxt(os.path.join(subdir_path, f), dtype=str, delimiter=',')
+            subdir_data = np.loadtxt(os.path.join(subdir_path, f), dtype=object, delimiter=',')
             data.append(subdir_data)
 
 np.savetxt("merged.csv", np.transpose(data), delimiter =", ", fmt ='% s')
