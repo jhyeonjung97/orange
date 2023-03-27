d921c7e4a87296c46fd0870a347b05ebf3c68957
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-27 17:58:44 +0900

    .

--- temp.sh
+++ temp.sh
@@ -7,6 +7,8 @@ else
         DIR='*/'
     elif [[ $1 == '-rr' ]]; then
         DIR='*/*/'
+    elif [[ $1 == '-rrr' ]]; then
+        DIR='*/*/*/'
     elif [[ $1 == '-s' ]] || [[ $1 == '-select' ]]; then
         DIR=${@:2}
     elif [[ -z $2 ]]; then
