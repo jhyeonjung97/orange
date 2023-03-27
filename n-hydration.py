d11b8c64fcfb8b99c551dcdf91c7a9186a46497a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-27 14:34:13 +0900

    .

---
+++ n-hydration.py
@@ -0,0 +1,55 @@
+from ase.io import read
+from ase.io.vasp import read_vasp_xdatcar
+import numpy as np
+
+# Define the cation symbols and their corresponding cutoff distances
+cation_cutoffs = {'Li': 2.5,
+                  'Na': 3.0,
+                  'K' : 3.5,
+                  'Rb': 3.8,
+                  'Cs': 4.1}
+
+# Read the XDATCAR file
+structures = read_vasp_xdatcar('XDATCAR', index=0)
+cell = structures[0].cell
+
+# Find the cation symbol
+cation = None
+for atom in structures[0]:
+    if atom.symbol in cation_cutoffs:
+        cation = atom.symbol
+        cutoff = cation_cutoffs[cation]
+        break
+else:
+    raise ValueError('Could not find cation in XDATCAR')
+
+min_z_positions = []
+for i, atoms in enumerate(structures):
+    # Get the indices of cation and water oxygen atoms
+    cation_indices = [j for j, atom in enumerate(atoms) if atom.symbol == cation]
+    water_oxygen_indices = [j for j, atom in enumerate(atoms) if atom.symbol == 'O']
+
+    min_z_position = None
+    for cation_index in cation_indices:
+        for water_oxygen_index in water_oxygen_indices:
+            # Calculate the distance between cation and water oxygen
+            dr = atoms[water_oxygen_index].position - atoms[cation_index].position
+
+            # Apply minimum image convention to account for periodic boundary conditions
+            for m in range(2):
+                if abs(dr[m]) > cell[m,m]/2:
+                    if dr[m] > 0:
+                        dr[m] -= cell[m,m]
+                    else:
+                        dr[m] += cell[m,m]
+
+            distance = np.linalg.norm(dr)
+
+            if distance <= cutoff:
+                numb_hydration += 1
+
+    numb_hydrations.append(numb_hydration)
+    print(f"Iteration {i}: {numb_hydration}")
+
+# Save the minimum z-positions as a csv file
+np.savetxt(f'numb_hydrations_{cation}.csv', numb_hydrations, delimiter=',')
