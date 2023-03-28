2d34766ea010b73f6cbedd41094873eb19a119eb
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-03-28 16:53:03 +0900

    .

--- send.sh
+++ send.sh
@@ -24,6 +24,9 @@ do
     elif [[ $i == 'burning' ]]; then
         port=' -P 1234'
         srvr='hyeonjung@burning.postech.ac.kr:'
+    elif [[ $i == 'x2421' ]]; then
+        port=''
+        srvr='x2421a10@nurion-dm.ksc.re.kr:'
     elif [[ $i == 'x2431' ]]; then
         port=''
         srvr='x2431a10@nurion-dm.ksc.re.kr:'
