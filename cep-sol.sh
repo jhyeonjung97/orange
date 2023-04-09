d9c5db60238c5f798911fd2da7c4f0c69e567e18
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-04-09 23:37:28 +0900

    .

--- cep-sol.sh
+++ cep-sol.sh
@@ -146,7 +146,7 @@ do
     echo -e "$ne\t$type\t$diff\t$sh\t$fl\t$wf\t$ep" >> cepout.log
     map+=([$ne]=$ep)
     mkdir cep_$ne
-    cp INCAR POSCAR CONTCAR XDATCAR OUTCAR OSZICAR vasprun.xml stdout.log cep_$ne
+    cp INCAR POSCAR CONTCAR XDATCAR CHGCAR OUTCAR OSZICAR vasprun.xml stdout.log cep_$ne
     x1=$x2
     y1=$y2
     x2=$ne
