f5087da2d78c1d0c01b9f1bc602f13044425f688
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-04-10 13:01:45 +0900

    .

--- resub.sh
+++ resub.sh
@@ -6,6 +6,7 @@ function resub {
     else
         rm STD* *out.log
     fi
+    rm WAVECAR CHGCAR
     # find . -size +10000000c -type f -delete
     if [[ -d pwscf.save ]]; then
         rm -r pwscf.save
