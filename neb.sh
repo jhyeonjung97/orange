7627274580a07f65221bfeaa33eb75e895b273d7
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-05-08 19:43:12 +0900

    .

--- neb.sh
+++ neb.sh
@@ -12,10 +12,10 @@ if [[ ${here} == 'burning' ]]; then
         echo 'prepare run_slurm.sh first..'
         exit 2
     fi
-    sed -i -e "s/nodes=1/nodes=$1" run_slurm.sh
+    sed -i -e "s/nodes=1/nodes=$1/" run_slurm.sh
 elif [[ ${here} == 'kisti' ]]; then
     sh ~/bin/orange/run-kisti.sh -l vtst
     np=$(echo $1 | awk '{print $1 * 64}')
-    sed -i -e "s/np 64/np $np" run_slurm.sh
-    sed -i -e "s/select=1/select=$1" run_slurm.sh
+    sed -i -e "s/np 64/np $np/" run_slurm.sh
+    sed -i -e "s/select=1/select=$1/" run_slurm.sh
 fi
