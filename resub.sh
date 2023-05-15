a51137c8ecf8fe3e5d3ba2b138b79ad2effc97cb
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-05-15 18:56:42 +0900

    .

--- resub.sh
+++ resub.sh
@@ -30,6 +30,10 @@ if [[ -z $1 ]]; then # simple re-submit
 else
     if [[ $1 == '-r' ]] || [[ $1 == 'all' ]]; then
         DIR='*/'
+    elif [[ $1 == '-rr' ]]; then
+        DIR='*/*/'
+    elif [[ $1 == '-rrr' ]]; then
+        DIR='*/*/*/'
     elif [[ $1 == '-s' ]] || [[ $1 == '-select' ]]; then
         DIR=${@:2}
     elif [[ -z $2 ]]; then
@@ -37,7 +41,6 @@ else
     else
         DIR=$(seq $1 $2)
     fi
-    
     for i in $DIR
     do
         i=${i%/}
