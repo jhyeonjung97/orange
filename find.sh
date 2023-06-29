64ee809596aa9d06faf936ac1d49debf626c137f
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-06-29 14:29:47 +0900

    .

--- find.sh
+++ find.sh
@@ -5,16 +5,16 @@ dir_now=$PWD
 while [[ $i -gt 0 ]]
 do
     i=0
-    for i in $DIR
+    for j in $DIR
     do
-        cd $i
-        if [[ -n $(grep $1 POTCAR) ]]; then
+        cd $j
+        if [[ -f POTCAR ]] && [[ -n $(grep $1 POTCAR) ]]; then
             pwd
             grep $1 POTCAR --color
         fi
         cd $dir_now
+        i+=1
     done
-    
     
     DIR=$DIR+'*/'
     echo "directories: $DIR"
