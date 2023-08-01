5afdb15f2ee06945c4ad91c38f6674fba41a3d7d
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-08-01 10:43:22 +0900

    .

--- spread.sh
+++ spread.sh
@@ -13,20 +13,29 @@ else
     DIR='*/'
 fi
 
-file=${@}
-for dir in $DIR
-do
-    cp $file $dir
-    # if [[ ! -e $file ]]; then
-    #     name=${file%.*}
-    #     ext=${file##*.}
-    #     numb=$(echo $dir | cut -c 1)
-    #     # echo $name $ext $numb
-    #     cp $name$numb.$ext $dir$name.$ext
-    # else
-    #     cp $file $dir
-    # fi
-    # if [[ $file == 'run_slurm.sh' ]]
-    #     cp .run_conti.sh $dir
-    # fi
-done
+if [[ $1 == '*.vasp' ]] && [[ ! -f $1 ]]; then
+    file=$1
+    name=${file%.*}
+    for i in $NUM
+    do
+        cp $name$i.vasp $i*/
+    done
+else
+    file=${@}
+    for dir in $DIR
+    do
+        cp $file $dir
+        # if [[ ! -e $file ]]; then
+        #     name=${file%.*}
+        #     ext=${file##*.}
+        #     numb=$(echo $dir | cut -c 1)
+        #     # echo $name $ext $numb
+        #     cp $name$numb.$ext $dir$name.$ext
+        # else
+        #     cp $file $dir
+        # fi
+        # if [[ $file == 'run_slurm.sh' ]]
+        #     cp .run_conti.sh $dir
+        # fi
+    done
+fi
