33d952c7ad76d2f1adbb95e30378c79e1a920740
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-09-05 14:56:42 +0900

    .

--- direct.sh
+++ direct.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 if [[ $1 == '-c' ]] || [[ $1 == '-n' ]]; then
-    ase convert --write-args direct=False -f $2 $2
-    echo "ase convert --write-args direct=False -f $2 $2"
+    shift
+    for file in $@
+    do
+        ase convert --write-args direct=False -f $file $file
+        echo "Cartesin $file"
+    done
 else
-    ase convert --write-args direct=True -f $1 $1
-    echo "ase convert --write-args direct=True -f $1 $1"
+    for file in $@
+    do
+        ase convert --write-args direct=True -f $file $file
+        echo "Direct $file"
+    done
 fi
