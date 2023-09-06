3695b95a340bde0b14190b052450fa25a5c2af0a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-09-06 21:41:43 +0900

    .

--- slab.py
+++ slab.py
@@ -1,4 +1,5 @@
 from sys import argv
+from os import system
 from ase.io import read, write
 from ase.build import surface
 from ase.constraints import FixAtoms
@@ -13,10 +14,10 @@ while i <= numb:
     # write(f'slab{i}.vasp',slab)
     # slab=read(f'slab{i}.vasp')
     write(f'slab{i}.vasp',slab.repeat((2,2,1)))
-    os.system(f'~/bin/pyband/xcell.py -i slab{i}.vasp -o xc{i}.vasp')
+    system(f'~/bin/pyband/xcell.py -i slab{i}.vasp -o xc{i}.vasp')
     xcell=read(f'xc{i}.vasp')
-    min_z=min(xcell.position[2])
-    fixed=FixAtoms(indices=[atom.index for atom in xcell if atom.position[2] <= min_z + 1])
+    min_z=float(min(xcell.positions[2]))
+    fixed=FixAtoms(indices=[atom.index for atom in xcell if atom.z <= min_z + 1])
     xcell.set_constraint(fixed)
     write(f'fix{i}.vasp',fixed)
     i=i+1
