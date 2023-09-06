6756eb52225a019676397a301e97599a4cb3173a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-09-06 22:00:18 +0900

    .

--- slab.py
+++ slab.py
@@ -7,6 +7,7 @@ from ase.constraints import FixAtoms
 filename=argv[1]
 numb=int(argv[2])
 vacuum=15.0
+boundary=1.0
 i=1
 
 while i <= numb:
@@ -16,14 +17,14 @@ while i <= numb:
     # write(f'slab{i}.vasp',slab)
     # slab=read(f'slab{i}.vasp')
     write(f'slab{i}.vasp',slab.repeat((2,2,1)))
-    system(f'~/bin/pyband/xcell.py -i slab{i}.vasp -o xc{i}.vasp')
+    system(f'python ~/bin/pyband/xcell.py -i slab{i}.vasp -o xc{i}.vasp')
     xcell=read(f'xc{i}.vasp')
     min_z=xcell.positions[:,2].min()
     # min_z=min([atom.position[2] for atom in xcell])
     # print(min_z)
     # del xcell.constraints
-    fixed=FixAtoms(indices=[atom.index for atom in xcell if atom.position[2] <= min_z+1])
-    print(fixed)
+    fixed=FixAtoms(indices=[atom.index for atom in xcell if atom.position[2] <= min_z+boundary])
+    # print(fixed)
     xcell.set_constraint(fixed)
     write(f'fix{i}.vasp',xcell)
     i=i+1
