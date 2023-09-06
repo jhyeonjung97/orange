8fcfce14929318452603268bee02fba476367183
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-09-06 22:09:34 +0900

    .

--- slab.py
+++ slab.py
@@ -24,7 +24,11 @@ while i <= numb:
     # min_z=min([atom.position[2] for atom in xcell])
     # print(min_z)
     # del xcell.constraints
-    fixed=FixAtoms(indices=[atom.index for atom in xcell if atom.position[2] <= min_z+boundary])
+    xcell.symbols[12]='Pt'
+    xcell.symbols[13]='Pt'
+    xcell.symbols[14]='Pt'
+    xcell.symbols[15]='Pt'
+    fixed=FixAtoms(indices=[atom.index for atom in xcell if atom.position[2] <= min_z + boundary])
     # print(fixed)
     xcell.set_constraint(fixed)
     write(f'fix{i}.vasp',xcell)
