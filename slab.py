459e58f21397a660ede812f47b3cd40428eb9d0a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-09-13 17:10:51 +0900

    .

--- slab.py
+++ slab.py
@@ -13,7 +13,7 @@ parser.add_argument('-o', '--output',type=str, default='slab.vasp', help='output
 parser.add_argument('-v', '--vacuum', type=float, default=20.0, help='Vaccum layer thickness (A)')
 parser.add_argument('-z', '--boundary', type=float, default=1.0, help='Boundary for fixed atoms (A)')
 parser.add_argument('-f', '--facet', type=str, default='1,1,1', help='Surface facet vector (e.g., "x,y,z")')
-parser.add_argument('-r', '--repeat', type=str, default='2,2,1', help='Repeat unit cell (e.g., "a,b,c")')
+parser.add_argument('-r', '--repeat', type=str, default='2,2', help='Repeat unit cell (e.g., "a,b")')
 parser.add_argument('-l', '--layer', type=int, default=3, help='the number of layers')
 
 # args, remaining_args = parser.parse_known_args()
@@ -36,7 +36,7 @@ if args.facet:
     x, y, z = map(int, args.facet.split(','))
     
 if args.repeat:
-    a, b, c = map(int, args.repeat.split(','))
+    a, b = map(int, args.repeat.split(','))
 # else:
 #     print('Repeat not provided.')
 
@@ -64,7 +64,7 @@ while i <= numb:
     # system(f'sh ~/bin/orange/rmv.sh slab{i}.vasp xc{i}.vasp')
     bulk=read(f'{filename}{i}.vasp')
     bulk.positions+=(0,0,bulk.cell[2,2]/2)
-    slab=surface(bulk, (x,y,z), layer, vacuum/2).repeat((a,b,c))
+    slab=surface(bulk, (x,y,z), layer, vacuum/2).repeat((a,b,1))
     slab.positions+=(0,0,0.1-vacuum/2)
     # write(f'slab{i}.vasp',slab)
     # slab=read(f'slab{i}.vasp')
