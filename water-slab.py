3699a3219c457e20e7c1a72dc23c964254b516ec
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-10-08 20:58:39 +0900

    .

--- water-slab.py
+++ water-slab.py
@@ -5,6 +5,7 @@ from ase.io import read, write
 parser = argparse.ArgumentParser(description='Command-line options example')
 
 parser.add_argument('filename', type=str, default='a.vasp', help='input filename (e.g., a for a1~a3.vasp)')
+parser.add_argument('-i', '--iteration', type=int, default=3, help='the number of files (e.g., 3 for a1~a3.vasp)')
 parser.add_argument('-n', '--number', type=int, default=0, help='the number of water molecules')
 parser.add_argument('-l', '--layer',type=int, default=4, help='the number of water layers')
 # parser.add_argument('-s', '--surface', type=int, default=9, help='the number of surface atoms')
@@ -16,31 +17,32 @@ args = parser.parse_args()
         
 # Process arguments parsed by argparse
 filename = args.filename
+iteration = args.iteration
 number = args.number
 layer = args.layer
 seed = args.seed
 output = args.output
 
-slab=read(filename)
-a=slab.cell[0][0]
-b=slab.cell[1][1]
-# c=slab.cell[2][2]
-z=slab.positions[:,2].max()
+for i in range(1,iteration+1):
+    slab=read(f'{filename}{i}.vasp')
+    a=slab.cell[0][0]
+    b=slab.cell[1][1]
+    # c=slab.cell[2][2]
+    z=slab.positions[:,2].max()
 
-factor=4/8.490373/4.901919 # ase WL.pj
-number=int(a*b*factor*layer)+1
-while number < 2:
-    print("How many water layers do you want?")
-    layer=input()
+    factor=4/8.490373/4.901919 # ase WL.pj
     number=int(a*b*factor*layer)+1
+    while number < 2:
+        print("How many water layers do you want?")
+        layer=input()
+        number=int(a*b*factor*layer)+1
 
-top=number/a/b*10**30/997/1000*18.01528/(6.022*10**23)
-# top=round(top, 4)
-system(f'sh ~/bin/orange/water-slab.sh {a} {b} {top} {number} {seed} {output}')
+    top=number/a/b*10**30/997/1000*18.01528/(6.022*10**23)
+    system(f'sh ~/bin/orange/water-slab.sh {a} {b} {top} {number} {seed} {output}')
 
-for i in range(1,seed+1):
-    water=read(f'{output}{i}.vasp')
-    water.positions+=(0, 0, z+2)
-    slab_water=slab+water
-    slab_water.wrap()
-    write(f'hello{i}.vasp',slab_water)
+    for j in range(1,seed+1):
+        water=read(f'{output}{j}.vasp')
+        water.positions+=(0, 0, z+2)
+        slab_water=slab+water
+        slab_water.wrap()
+        write(f'{filename}{i}{j}.vasp',slab_water)
