a0e4113cd6a8aae5b65f186094e43cd077748ecd
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-10-08 20:30:14 +0900

    .

---
+++ water-slab.py
@@ -0,0 +1,33 @@
+import argparse
+from os import system
+from ase.io import read, write
+
+parser = argparse.ArgumentParser(description='Command-line options example')
+
+parser.add_argument('filename', type=str, default='a.vasp', help='input filename (e.g., a for a1~a3.vasp)')
+parser.add_argument('-n', '--number', type=int, default=0, help='the number of water molecules')
+parser.add_argument('-l', '--layer',type=int, default=4, help='the number of water layers')
+# parser.add_argument('-s', '--surface', type=int, default=9, help='the number of surface atoms')
+parser.add_argument('-s', '--seed', type=int, default=3, help='the number of seeds')
+parser.add_argument('-o', '--output', type=str, default='water-slab', help='output filename')
+
+slab=read(f'{filename}')
+a=slab.cell[0][0]
+b=slab.cell[1][1]
+c=slab.cell[2][2]
+z=slab.positions[:,2].max()+2
+
+factor=4/8.490373/4.901919 # ase WL.pj
+number=int(a*b*factor*layer)+1
+while number < 2:
+    print("How many water layers do you want?")
+    layer=input()
+    number=int(a*b*factor*layer)+1
+
+top=z+number/a/b*10**30/997/1000*18.01528/(6.022*10**23)
+top=round(top, 3)
+system(f'sh ~/bin/orange/water-slab.sh {a} {b} {c} {z} {top} {number} {seed} {output}')
+
+for i in range(1,seed+1):
+    water=read(f'{output}{i}.vasp')
+    write(f'{output}{i}.vasp',slab+water)
---
+++ water-slab.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ $1 =~ '-h' ]] || [[ $1 == '*.inp' ]] || [[ -z $2 ]]; then
+    # echo 'usage: sh ~/bin/orange/water-slab.sh {a} {b} {c} {z} {top} {number} {seed} {output}'
+    exit 1
+fi
+
+a=$1; shift
+b=$1; shift
+c=$1; shift
+z=$1; shift
+top=$1; shift
+number=$1; shift
+seed=$1; shift
+output=$1; shift
+
+cp ~/input_files/water.xyz .
+cp ~/input_files/water-slab.inp .
+sed -i "/number/c\number $number" mixture.inp
+sed -i "/inside/c\inside box 0. 0. $z $a $b $top" mixture.inp
+
+for i in $(seq 1 $seed)
+do
+    sed -i "/output/c\output $output$i.xyz" water-slab.inp
+    sed -i "/seed/c\seed $i" water-slab.inp
+    ~/bin/packmol/packmol < water-slab.inp
+done
+
+python3 ~/bin/orange/convert.py xyz vasp $a $b $c
