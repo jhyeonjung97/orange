2db12652c0cb3ae2fee018874cd06d4ba9559615
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-10-12 18:44:25 +0900

    .

--- vacuum.py
+++ vacuum.py
@@ -5,5 +5,5 @@ vac=float(argv[1])
 for a in ['a', 'b', 'c']:
     for i in range(0,9):
         atoms=read(f'{a}{i}.vasp')
-        atoms.cell[2][2]+=vac
+        atoms.cell[2][2]=vac
         write(f'{a}{i}.vasp',atoms)
