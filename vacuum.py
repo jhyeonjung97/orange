e817b93ad0ea739dc467a6626bc78ac9181f982c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-10-12 20:23:41 +0900

    .

--- vacuum.py
+++ vacuum.py
@@ -2,8 +2,12 @@ from ase.io import read, write
 from sys import argv
 
 vac=float(argv[1])
-for a in ['a', 'b', 'c']:
-    for i in range(0,9):
-        atoms=read(f'{a}{i}.vasp')
-        atoms.cell[2][2]=vac
-        write(f'{a}{i}.vasp',atoms)
+# for a in ['a', 'b', 'c']:
+#     for i in range(0,9):
+#         atoms=read(f'{a}{i}.vasp')
+#         atoms.cell[2][2]=vac
+#         write(f'{a}{i}.vasp',atoms)
+
+atoms=read('POSCAR')
+atoms.cell[2][2]=vac
+write('POSCAR',atoms)
