9a927a343751f9164a28e2fb7608f6bb2f60460e
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-10-12 18:42:31 +0900

    .

--- vacuum.py
+++ vacuum.py
@@ -1,7 +1,9 @@
 from ase.io import read, write
+from sys import argv
 
+vac=float(argv[1])
 for a in ['a', 'b', 'c']:
     for i in range(0,9):
         atoms=read(f'{a}{i}.vasp')
-        atoms.cell[2][2]+=5.0
+        atoms.cell[2][2]+=vac
         write(f'{a}{i}.vasp',atoms)
