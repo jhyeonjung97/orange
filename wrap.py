ccf93235f0cb5c176ded19e067acfc4e8d5470b0
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-10-13 17:47:19 +0900

    .

--- wrap.py
+++ wrap.py
@@ -2,13 +2,16 @@ from ase.io import read, write
 from sys import argv
 
 filename=argv[1]
-height=float(argv[2])
+atoms=read(f'{filename}')
+
 # for a in ['a', 'b', 'c']:
 #     for i in range(0,9):
 #         atoms=read(f'{a}{i}.vasp')
 #         atoms.cell[2][2]=vac
 #         write(f'{a}{i}.vasp',atoms)
 
-atoms=read(f'{filename}')
-atoms.cell[2][2]=height
+if len(argv)==2:
+    height=float(argv[2])
+    atoms.cell[2][2]=height
+
 write(f'{filename}',atoms)
