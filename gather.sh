af682051e943fd447d3ab0182c8c97eed1d77f74
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-10-25 17:46:41 +0900

    .

--- gather.sh
+++ gather.sh
@@ -108,7 +108,7 @@ do
             if [[ $pattern == 'POSCAR' ]] || [[ $pattern == 'CONTCAR' ]]; then
                 if [[ $f == 'POSCAR' ]]; then
                     cp POSCAR $destination$filename$numb.vasp
-                    echo "$dir'POSCAR' $filename$numb.vasp"
+                    echo "$dir$f $filename$numb.vasp"
                 elif [[ $pattern == 'POSCAR' ]] && [[ -e initial.vasp ]]; then
                     cp initial.vasp $destination$filename$numb.vasp
                     echo "$dir'initial.vasp' $filename$numb.vasp"
