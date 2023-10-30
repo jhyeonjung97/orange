4d010ac57d01f65c2514f06c7c4361a218f1536b
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-10-30 09:35:31 +0900

    .

--- names.sh
+++ names.sh
@@ -4,8 +4,9 @@ echo -n 'final names: '
 read -a f
 
 n=${#i[@]}
+n=$(($n-1))
 for j in $(seq 0 $n)
 do
-    echo "mv ${i[$j]} $f{[$j]}"
-    mv ${i[$j]} $f{[$j]}
+    echo "mv ${i[$j]} ${f[$j]}"
+    mv ${i[$j]} ${f[$j]}
 done
