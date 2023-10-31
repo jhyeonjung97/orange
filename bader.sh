3337c8df7e1479c45698946277ad4c0ec49489e3
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-10-31 10:55:28 +0900

    .

--- bader.sh
+++ bader.sh
@@ -1,19 +1,15 @@
-function bader {
+function bader-chg {
     chgsum.pl AECCAR0 AECCAR2
     bader CHGCAR -ref CHGCAR_sum
-    }
+}
 
-function directory {
-    if ! [[ $2 == '-r' ]]; then
-        $1
-    else
-        for dir in */
-        do
-            cd $dir
-            $1
-            cd ..
-        done
-    fi
-    }
-    
-directory $(bader) $1
+if ! [[ $1 == '-r' ]]; then
+    bader-chg
+else
+    for dir in */
+    do
+        cd $dir
+        bader-chg
+        cd ..
+    done
+fi
