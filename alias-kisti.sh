f8e5f9b9c90b392987327ba25b54d859d10f36aa
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-11-03 14:00:46 +0900

    .

--- alias-kisti.sh
+++ alias-kisti.sh
@@ -6,6 +6,6 @@ alias pestat='pbs_status | head -n 2
 pbs_status | grep normal
 pbs_status | grep norm_skl
 pbs_status | grep long'
-alias mystat="qstat -u ${account}"
+alias mystat="qstat -T -u ${account}"
 alias ta='tail -n 6 */stdout.log'
 alias taa='tail -n 6 */*/stdout.log'
