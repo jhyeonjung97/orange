149bf75a56273f76a676a47ac90269d97b2b183a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2023-11-03 14:50:37 +0900

    .

--- chg.sh
+++ chg.sh
@@ -1,2 +1,19 @@
-sh ~/bin/orange/modify.sh INCAR LCHARG True
-sh ~/bin/orange/modify.sh INCAR LAECHG True
+function chg { 
+    sh ~/bin/orange/modify.sh INCAR LCHARG True
+    sh ~/bin/orange/modify.sh INCAR LAECHG True
+}
+
+
+if [[ $1 == '-r' ]]; then
+    for dir in */
+    do
+        cd $dir
+        bader
+        cd ..
+    done
+else
+    bader
+fi
+    
+
+
