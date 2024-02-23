1fd3ffbf56620abdd65a683168159882ca02ea1f
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-02-23 15:29:47 -0800

    .

--- magmom-anti.py
+++ magmom-anti.py
@@ -90,8 +90,9 @@ MAGMOMlist=[]
 for a , i in enumerate(line62) :
     try :
         atom=iMAGMOM['{}'.format(i)]
-        MAGMOMlist.append(atomnumber[a]/2+"*"+atom)
-        MAGMOMlist.append(atomnumber[a]/2+"*-"+atom)
+        atomnumber_half=string(int(atomnumber[a])/2)
+        MAGMOMlist.append(atomnumber_half+"*"+atom)
+        MAGMOMlist.append(atomnumber_half+"*-"+atom)
     except:
         MAGMOMlist.append(atomnumber[a]+"*"+'0')
 print(MAGMOMlist)
