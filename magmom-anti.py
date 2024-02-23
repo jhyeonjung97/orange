528f1abe8abdde289b563b27a5dca807ce14503c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-02-23 15:39:08 -0800

    .

--- magmom-anti.py
+++ magmom-anti.py
@@ -90,7 +90,7 @@ MAGMOMlist=[]
 for a , i in enumerate(line62) :
     try :
         atom=iMAGMOM['{}'.format(i)]
-        b=float(atomnumber[a])/2
+        b=int(atomnumber[a])/2
         print(b)
         MAGMOMlist.append(atomnumber[a]+"*"+atom)
     except:
