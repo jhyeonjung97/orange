5c815c75a7f8dbff4929af776fcf1f69a683b5ce
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-02-23 17:22:12 -0800

    .

--- ldauu.py
+++ ldauu.py
@@ -85,11 +85,10 @@ atomline=' '.join(atomlist)
 
 if ldau_tag == 0 :
     subprocess.call('sed -i \'/LDAU /c\LDAU = .FALSE.\' INCAR', shell=True)
-
-subprocess.call('sed -i \'/LDAUU/d\' INCAR', shell=True)
-subprocess.call('sed -i \'/LDAUL/aLDAUU = '+firstline+' \# '+atomline+'\' INCAR', shell=True)
-
-print(Bu)
-subprocess.call('grep \'LDAU =\' INCAR', shell=True)
-subprocess.call('grep LDAUU INCAR', shell=True)
-print(reset)
+else :
+    subprocess.call('sed -i \'/LDAUU/d\' INCAR', shell=True)
+    subprocess.call('sed -i \'/LDAUL/aLDAUU = '+firstline+' \# '+atomline+'\' INCAR', shell=True)
+    print(Bu)
+    subprocess.call('grep \'LDAU =\' INCAR', shell=True)
+    subprocess.call('grep LDAUU INCAR', shell=True)
+    print(reset)
