212b74dba1f15ee921fdf068a2bcb87b6c322e5b
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-02-29 09:21:38 -0800

    .

--- alias-nersc.sh
+++ alias-nersc.sh
@@ -3,6 +3,8 @@
 alias cdw='cd /pscratch/sd/j/jiuy97'
 alias cdp='cd /global/cfs/cdirs/m2997/'
 
+alias vasp5='mv /global/homes/j/jiuy97/.local/lib/python3.11/site-packages/ase/io/vasp_parsers/vasp_outcar_parsers5.py /global/homes/j/jiuy97/.local/lib/python3.11/site-packages/ase/io/vasp_parsers/vasp_outcar_parsers.py'
+alias vasp6='mv /global/homes/j/jiuy97/.local/lib/python3.11/site-packages/ase/io/vasp_parsers/vasp_outcar_parsers6.py /global/homes/j/jiuy97/.local/lib/python3.11/site-packages/ase/io/vasp_parsers/vasp_outcar_parsers.py'
 alias run='sh run-nersc.sh'
 alias time='grep sec OUTCAR'
 alias qdel='scancel'
