3509fa0437384d9e0002370b126946cf941fa400
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-04 18:15:43 -0800

    .

--- mmff.sh
+++ mmff.sh
@@ -8,7 +8,6 @@ fi
 name="${1%.*}"
 ext="${1##*.}"
 shift
-# echo $name $ext
 
 a=$1
 b=$2
@@ -20,35 +19,6 @@ if [[ -z $a ]]; then
     c=40.
 fi
 
-# if [[ -f $name.$ext ]]; then
-#     # python ~/bin/orange/cluster.py $name.$ext $name.xyz $a
-#     # obabel $name.xyz -O $name.mol2
-#     if [[ $ext != 'mol2' ]]; then
-#         obabel $name.$ext -O $name.mol2
-#     fi
-#     obminimize -n 10000000000 -sd -c 1e-10 -ff MMFF94s $name.mol2 > $name.pdb
-# fi
-    
-# for file in *
-# do
-#     name0="${file%.*}"
-#     ext0="${file##*.}"
-#     # echo $name0 $ext0
-#     if [[ $name0 =~ $name ]] && [[ $ext0 == $ext ]]; then
-#         # python ~/bin/orange/cluster.py $name$i.$ext $name$i.xyz $a
-#         # obabel $name$i.xyz -O $name$i.mol2
-#         i=${name0//$name/}
-#         if [[ $ext != 'mol2' ]]; then
-#             echo "obabel $name$i.$ext -O $name$i.mol2"
-#             obabel $name$i.$ext -O $name$i.mol2
-#         fi
-#         echo "obminimize -n 10000000000 -sd -c 1e-10 -ff MMFF94s $name$i.mol2 > $name$i.pdb"
-#         obminimize -n 10000000000 -sd -c 1e-10 -ff MMFF94s $name$i.mol2 > $name$i.pdb
-#         # obminimize -n 10000000000 -sd -c 1e-10 -ff MMFF94s a1.mol2 > a1.pdb
-#         # python ~/bin/orange/cluster.py $name$i.pdb $name$i.xyz $a
-#     fi
-# done
-
 for i in {0..9}
 do
     echo "obabel $name$i.$ext -O $name$i.mol2"
@@ -57,7 +27,7 @@ do
     obminimize -n 100000000 -sd -c 1e-8 -ff MMFF94s $name$i.mol2 > $name$i.pdb
 done
 
-python3 ~/bin/orange/convert.py pdb vasp $a $b $c
+python3 ~/bin/orange/convert.py pdb json $a $b $c
 
 if [[ -n $(grep mmff.sh run_slurm.sh) ]] && [[ -n $(grep mpiexe run_slurm.sh) ]]; then
     cp $name$i.vasp POSCAR
