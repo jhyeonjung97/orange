699203941bec2b760c1d83424cadeed3a6d1b7f7
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-29 14:22:08 -0700

    .

--- backup.sh
+++ backup.sh
@@ -28,7 +28,6 @@ find . -name OSZICAR -type f -delete
 find . -name XDATCAR -type f -delete
 find . -name WAVECAR -type f -delete
 find . -name EIGENVAL -type f -delete
-find . -name OUTCAR -type f -exec ase convert {} final_with_calculator.json \;
 find . -name 'conti*' -type d -exec rm -rv {} \;
 find . -name x -type d -exec rm -rv {} \;
 find . -empty -delete
