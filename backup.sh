9be7d7e19b2f8d3cf29bbae1000c47a8c29c13e4
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-29 13:02:44 -0700

    .

--- backup.sh
+++ backup.sh
@@ -7,16 +7,16 @@ if [[ ! yn == y* ]]; then
     exit 1
 fi
 
-if [[ ${here} == kisti ]]; then
-    echo "cp -r $PWD /scratch/${account}/backup"
-    cp -r $PWD /scratch/${account}/backup
-elif [[ ${here} =~ burning ]] || [[ ${here} == mac ]] || [[ ${here} == mini ]]; then
-    echo "cp -r $PWD ~/backup"
-    cp -r $PWD ~/backup
-else
-    echo 'where are you?'
-    exit 2
-fi
+# if [[ ${here} == kisti ]]; then
+#     echo "cp -r $PWD /scratch/${account}/backup"
+#     cp -r $PWD /scratch/${account}/backup
+# elif [[ ${here} =~ burning ]] || [[ ${here} == mac ]] || [[ ${here} == mini ]]; then
+#     echo "cp -r $PWD ~/backup"
+#     cp -r $PWD ~/backup
+# else
+#     echo 'where are you?'
+#     exit 2
+# fi
 
 find . -name 'CHG*' -type f -delete
 find . -name 'DOS*' -type f -delete
